aeff817b609881decf941222d27066340e22261f
czerwingithub <czerwin@scalyr.com>
2015-01-17 17:45:13 -0800

    Added in an abstraction to help with an optimiztion to efficiently calculate the size of the post fix buffer.  This helps fix the CPU consumption problem with saw with a customer who had a large number of unchanging files.

--- scalyr_agent/tests/scalyr_client_test.py
+++ scalyr_agent/tests/scalyr_client_test.py
@@ -19,7 +19,7 @@ __author__ = 'czerwin@scalyr.com'
 
 import unittest
 
-from scalyr_agent.scalyr_client import AddEventsRequest
+from scalyr_agent.scalyr_client import AddEventsRequest, PostFixBuffer
 
 
 class AddEventsRequestTest(unittest.TestCase):
@@ -150,3 +150,89 @@ class AddEventsRequestTest(unittest.TestCase):
             """{"token":"fakeToken", events: [{"name":"eventOne","ts":"1"},{"name":"eventTwo","ts":"2"}]"""
             """, threads: [], client_time: 2 }""")
         request.close()
+
+
+class PostFixBufferTest(unittest.TestCase):
+
+    def setUp(self):
+        self.__format = '], threads: THREADS, client_time: TIMESTAMP }'
+
+    def test_basic_case(self):
+        test_buffer = PostFixBuffer(self.__format)
+        test_buffer.set_client_timestamp(1)
+
+        test_buffer.add_thread_entry('log_5', 'histogram_builder')
+        self.assertEquals(test_buffer.content(),
+                          """], threads: [{"id":"log_5","name":"histogram_builder"}], client_time: 1 }""")
+
+    def test_set_client_time(self):
+        test_buffer = PostFixBuffer(self.__format)
+        test_buffer.set_client_timestamp(1)
+        self.assertTrue(test_buffer.set_client_timestamp(433423))
+
+        expected_length = test_buffer.length
+
+        content = test_buffer.content(cache_size=False)
+
+        self.assertEquals(content, '], threads: [], client_time: 433423 }')
+        self.assertEquals(expected_length, len(content))
+
+    def test_set_client_time_fail(self):
+        test_buffer = PostFixBuffer(self.__format)
+        self.assertTrue(test_buffer.set_client_timestamp(1, fail_if_buffer_exceeds=1000000))
+        self.assertFalse(test_buffer.set_client_timestamp(433423, fail_if_buffer_exceeds=test_buffer.length+3))
+
+        expected_length = test_buffer.length
+
+        content = test_buffer.content(cache_size=False)
+
+        self.assertEquals(content, '], threads: [], client_time: 1 }')
+        self.assertEquals(expected_length, len(content))
+
+    def test_add_thread(self):
+        test_buffer = PostFixBuffer(self.__format)
+        test_buffer.set_client_timestamp(1)
+
+        self.assertTrue(test_buffer.add_thread_entry('log_5', 'histogram_builder'))
+        self.assertEquals(test_buffer.length, len(test_buffer.content(cache_size=False)))
+
+        self.assertTrue(test_buffer.add_thread_entry('log_12', 'ok_builder'))
+        self.assertEquals(test_buffer.length, len(test_buffer.content(cache_size=False)))
+
+        self.assertTrue(test_buffer.add_thread_entry('log', 'histogram_builder_foo'))
+        self.assertEquals(test_buffer.length, len(test_buffer.content(cache_size=False)))
+
+        self.assertEquals(test_buffer.content(), """], threads: [{"id":"log_5","name":"histogram_builder"},"""
+                                                 """{"id":"log_12","name":"ok_builder"},"""
+                                                 """{"id":"log","name":"histogram_builder_foo"}], client_time: 1 }""")
+
+    def test_add_thread_fail(self):
+        test_buffer = PostFixBuffer(self.__format)
+        test_buffer.set_client_timestamp(1)
+
+        self.assertTrue(test_buffer.add_thread_entry('log_5', 'histogram_builder', fail_if_buffer_exceeds=1000000))
+        self.assertEquals(test_buffer.length, len(test_buffer.content(cache_size=False)))
+
+        self.assertFalse(test_buffer.add_thread_entry('log_6', 'histogram', fail_if_buffer_exceeds=10))
+
+        self.assertEquals(test_buffer.length, len(test_buffer.content(cache_size=False)))
+
+        self.assertEquals(test_buffer.content(), """], threads: [{"id":"log_5","name":"histogram_builder"}], """
+                                                 """client_time: 1 }""")
+
+    def test_set_position(self):
+        test_buffer = PostFixBuffer(self.__format)
+        test_buffer.set_client_timestamp(1)
+
+        self.assertTrue(test_buffer.add_thread_entry('log_5', 'histogram_builder'))
+
+        position = test_buffer.position
+
+        self.assertTrue(test_buffer.add_thread_entry('log_6', 'histogram2_builder'))
+
+        test_buffer.set_position(position)
+
+        self.assertEquals(test_buffer.length, len(test_buffer.content(cache_size=False)))
+
+        self.assertEquals(test_buffer.content(), """], threads: [{"id":"log_5","name":"histogram_builder"}], """
+                                                 """client_time: 1 }""")
