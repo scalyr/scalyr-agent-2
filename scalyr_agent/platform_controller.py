08a85361bf1825604a7d4311c772614a88825d0f
Scott Sullivan <scott.sullivan@ziften.com>
2015-01-20 15:54:27 +0000

    Defined enumeration for Windows MSI installation

--- scalyr_agent/platform_controller.py
+++ scalyr_agent/platform_controller.py
@@ -27,6 +27,7 @@ from __scalyr__ import get_install_root
 PACKAGE_INSTALL = 1    # Indicates source code was installed via a package manager such as RPM or Windows executable.
 TARBALL_INSTALL = 2    # Indicates source code was installed via a tarball created by the build_package.py script.
 DEV_INSTALL = 3        # Indicates source code is running out of the original source tree, usually during dev testing.
+MSI_INSTALL = 4        # Indicates source code was installed via a Windows MSI package
 
 
 class PlatformController:
