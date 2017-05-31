6031696e50cc3c1d37489097ad02cc222ed9841e
Imron Alston <imron@imralsoftware.com>
2017-05-31 15:28:41 +0000

    keeping track of active log files

--- scalyr_agent/configuration.py
+++ scalyr_agent/configuration.py
@@ -563,6 +563,11 @@ class Configuration(object):
         return self.__get_config().get_float('global_monitor_sample_interval')
 
     @property
+    def full_checkpoint_interval(self):
+        """Returns the configuration value for 'full_checkpoint_interval_in_seconds'."""
+        return self.__get_config().get_int('full_checkpoint_interval_in_seconds' )
+
+    @property
     def close_old_files_duration_in_seconds(self):
         """Returns the configuration value for 'close_old_files_duration_in_seconds'."""
         return self.__get_config().get_int('close_old_files_duration_in_seconds')
@@ -844,6 +849,7 @@ class Configuration(object):
 
         self.__verify_or_set_optional_float(config, 'global_monitor_sample_interval', 30.0, description, apply_defaults)
         self.__verify_or_set_optional_int(config, 'close_old_files_duration_in_seconds', 60*60*1, description, apply_defaults)
+        self.__verify_or_set_optional_int(config, 'full_checkpoint_interval_in_seconds', 60, description, apply_defaults)
 
         self.__verify_or_set_optional_int(config, 'max_allowed_request_size', 1*1024*1024, description, apply_defaults)
         self.__verify_or_set_optional_int(config, 'min_allowed_request_size', 100*1024, description, apply_defaults)
