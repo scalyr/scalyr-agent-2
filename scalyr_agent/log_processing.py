3442999fbe45c46dc92a43953df7df2535b26841
Imron Alston <imron@imralsoftware.com>
2018-09-05 04:40:40 +0800

    handle potentially truncated utf8 in error message when log is very, very long

--- scalyr_agent/log_processing.py
+++ scalyr_agent/log_processing.py
@@ -521,7 +521,7 @@ class LogFileIterator(object):
 
             except Exception, e:
                 # something went wrong. Return the full line and log a message
-                log.warn("Error parsing line as json for %s.  Logging full line: %s\n%s" % (self.__path, str(e), result.line),
+                log.warn("Error parsing line as json for %s.  Logging full line: %s\n%s" % (self.__path, str(e), result.line.decode( "utf-8", 'replace' )),
                          limit_once_per_x_secs=300, limit_key=('bad-json-%s' % self.__path))
         return result
 
