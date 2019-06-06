072f4df1cad8e4e41e05c399816756ccdb1e32b8
Edward Chee <echee@scalyr.com>
2019-06-05 18:06:25 -0700

    Remove singleton

--- scalyr_agent/tests/util_test.py
+++ scalyr_agent/tests/util_test.py
@@ -731,7 +731,7 @@ def rate_maintainer(consecutive_success_threshold, backoff_rate, increase_rate):
 class BlockingRateLimiterTest(ScalyrTestCase):
 
     def setUp(self):
-        self._fake_clock = scalyr_util.FakeClock()
+        self._fake_clock = scalyr_util.RealFakeClock()
 
     def test_fixed_rate_single_concurrency(self):
         """Longer experiment"""
