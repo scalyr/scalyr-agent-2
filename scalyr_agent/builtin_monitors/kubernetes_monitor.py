5aaca21df688bd935b861dff4ac98f7f41286bed
Edward Chee <echee@scalyr.com>
2019-06-11 11:42:22 -0700

    Log ratelimit increase/backoff

--- scalyr_agent/builtin_monitors/kubernetes_monitor.py
+++ scalyr_agent/builtin_monitors/kubernetes_monitor.py
@@ -2213,6 +2213,7 @@ class KubernetesMonitor( ScalyrMonitor ):
             self._global_config.k8s_ratelimit_increase_factor,
             self._global_config.k8s_ratelimit_backoff_factor,
             self._global_config.k8s_ratelimit_max_concurrency,
+            logger=global_log,
         )
 
         self.__controlled_warmer = None
