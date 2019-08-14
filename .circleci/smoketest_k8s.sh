a8bdc3bbb3badb764231716ee495df2ca01b6ac3
Edward Chee <echee@scalyr.com>
2019-08-14 13:13:25 -0700

    Harden k8s smoketest script

--- .circleci/smoketest_k8s.sh
+++ .circleci/smoketest_k8s.sh
@@ -36,6 +36,22 @@ max_wait=$2
 # Smoketest code (built into smoketest image)
 smoketest_script="source ~/.bashrc && pyenv shell 3.7.3 && python3 /tmp/smoketest.py"
 
+
+# container names for all test containers
+# The suffixes MUST be one of (agent, uploader, verifier) to match verify_upload::DOCKER_CONTNAME_SUFFIXES
+contname_agent="ci-agent-k8s-${CIRCLE_BUILD_NUM}-agent"
+contname_uploader="ci-agent-k8s-${CIRCLE_BUILD_NUM}-uploader"
+contname_verifier="ci-agent-k8s-${CIRCLE_BUILD_NUM}-verifier"
+
+
+# Delete existing resources
+kubectl delete deployment ${contname_verifier}
+kubectl delete deployment ${contname_uploader}
+kubectl delete daemonset scalyr-agent-2
+kubectl delete configmap scalyr-config
+kubectl delete secret scalyr-api-key
+kubectl delete -f https://raw.githubusercontent.com/scalyr/scalyr-agent-2/release/k8s/scalyr-service-account.yaml
+
 # Create service account
 kubectl create -f https://raw.githubusercontent.com/scalyr/scalyr-agent-2/release/k8s/scalyr-service-account.yaml
 
@@ -53,20 +69,15 @@ kubectl create configmap scalyr-config \
 # Build local image (add .ci.k8s to version)
 TEMP_DIRECTORY=~/temp_directory
 mkdir $TEMP_DIRECTORY
+/bin/rm -rf $TEMP_DIRECTORY/*
 perl -pi.bak -e 's/\s*(\S+)/$1\.ci\.k8s/' VERSION
+pwd
 python build_package.py k8s_builder
 TARBALL=$(ls scalyr-k8s-agent-*)
 mv $TARBALL $TEMP_DIRECTORY
 pushd $TEMP_DIRECTORY
 ./${TARBALL} --extract-packages
 docker build -t local_k8s_image .
-
-
-# container names for all test containers
-# The suffixes MUST be one of (agent, uploader, verifier) to match verify_upload::DOCKER_CONTNAME_SUFFIXES
-contname_agent="ci-agent-k8s-${CIRCLE_BUILD_NUM}-agent"
-contname_uploader="ci-agent-k8s-${CIRCLE_BUILD_NUM}-uploader"
-contname_verifier="ci-agent-k8s-${CIRCLE_BUILD_NUM}-verifier"
 
 
 # Create DaemonSet, referring to local image.  Launch agent.
@@ -78,10 +89,17 @@ kubectl create -f scalyr-agent-2-envfrom.yaml
 agent_hostname=$(kubectl get pods | fgrep scalyr-agent-2 | awk {'print $1'})
 echo "Agent pod == ${agent_hostname}"
 
-
 # Launch Uploader container (only writes to stdout, but needs to query Scalyr to verify agent liveness)
 # You MUST provide scalyr server, api key and importantly, the agent_hostname container ID for the agent-liveness
 # query to work (uploader container waits for agent to be alive before uploading data)
+echo ""
+echo "=================================================="
+echo "Starting uploader"
+echo "=================================================="
+former_uploader_hostname=$(kubectl get pods | fgrep ${contname_uploader} | awk {'print $1'})
+if [[ -z $former_uploader_hostname ]]; then
+    former_uploader_hostname='NO_SUCH_UPLOADER'
+fi
 kubectl run ${contname_uploader} --image=${smoketest_image} -- \
 bash -c "${smoketest_script} \
 ${contname_uploader} ${max_wait} \
@@ -89,13 +107,18 @@ ${contname_uploader} ${max_wait} \
 --scalyr_server ${SCALYR_SERVER} \
 --read_api_key ${READ_API_KEY} \
 --agent_hostname ${agent_hostname}"
+
 # Capture uploader pod
-uploader_hostname=$(kubectl get pods | fgrep ${contname_uploader} | awk {'print $1'})
+uploader_hostname=$(kubectl get pods | fgrep ${contname_uploader} | fgrep -v ${former_uploader_hostname} | awk {'print $1'})
 echo "Uploader pod == ${uploader_hostname}"
 
 
 # Launch synchronous Verifier image (writes to stdout and also queries Scalyr)
 # Like the Uploader, the Verifier also waits for agent to be alive before uploading data
+echo ""
+echo "=================================================="
+echo "Starting verifier"
+echo "=================================================="
 kubectl run -it ${contname_verifier} --image=${smoketest_image} -- \
 bash -c "${smoketest_script} \
 ${contname_verifier} ${max_wait} \
