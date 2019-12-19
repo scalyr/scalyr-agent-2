6925dc15b30dea82a341f60d9038081539b4b4e7
Steven Czerwinski <czerwin@scalyr.com>
2019-12-19 14:57:55 -0800

    Fix bug

--- scalyr_agent/scalyr_client.py
+++ scalyr_agent/scalyr_client.py
@@ -595,10 +595,10 @@ class ScalyrClientSession(object):
         }
 
         if session_info is not None:
-            body["sessionInfo"] = session_info
+            # session_info used to be a JsonObject but now must be dict.
+            assert type(session_info) == dict
 
-        # session_info used to be a JsonObject but now must be dict.
-        assert type(session_info) == dict
+            body["sessionInfo"] = session_info
 
         return AddEventsRequest(
             body,
