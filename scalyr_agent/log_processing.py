525e2ac9d81f6eb3365e382f8ff1a39b6b9c755e
czerwingithub <czerwin@scalyr.com>
2019-12-26 23:08:30 -0800

    Remove UUID third party lib (#354)
    Python 2.5 and higher include uuid so we can remove it from third party.

--- scalyr_agent/log_processing.py
+++ scalyr_agent/log_processing.py
@@ -37,11 +37,10 @@ import string
 import threading
 import time
 import timeit
+import uuid
 
 import scalyr_agent.scalyr_logging as scalyr_logging
 import scalyr_agent.util as scalyr_util
-
-import scalyr_agent.third_party.uuid_tp.uuid as uuid
 
 from scalyr_agent.agent_status import LogMatcherStatus
 from scalyr_agent.agent_status import LogProcessorStatus
