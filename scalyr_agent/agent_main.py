127607407e2a5a748e7e13fefe5ecf942fb0fa46
Tomaz Muraus <tomaz@tomaz.me>
2020-02-13 21:51:56 +0100

    Throw a more user-friendly exception and fail early and loudly if either ca cert or intermediate certs file doesn't exist.
    Previosly a somewhat cryptic exception was thrown.

--- scalyr_agent/agent_main.py
+++ scalyr_agent/agent_main.py
@@ -1207,6 +1207,17 @@ class ScalyrAgent(object):
         if self.__config.verify_server_certificate:
             ca_file = self.__config.ca_cert_path
             intermediate_certs_file = self.__config.intermediate_certs_path
+
+            # Validate provided CA cert file and intermediate cert file exists. If they don't
+            # exist, throw and fail early and loudly
+            if not os.path.isfile(ca_file):
+                raise ValueError(("Invalid path \"%s\" specified for the \"ca_cert_path\" config "
+                                  "option: file doesn't exist" % (ca_file)))
+
+            if not os.path.isfile(intermediate_certs_file):
+                raise ValueError(("Invalid path \"%s\" specified for the "
+                                  "\"intermediate_certs_path\" config "
+                                  "option: file doesn't exist" % (intermediate_certs_file)))
         else:
             ca_file = None
             intermediate_certs_file = None
