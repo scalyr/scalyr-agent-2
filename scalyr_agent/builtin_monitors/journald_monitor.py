8aa861d2373e4e80ac07d7c7e9352f587d6555e2
Tomaz Muraus <tomaz@tomaz.me>
2020-03-20 15:57:06 +0100

    Update class docstrings for built-in monitors so they contain latest up to date documentation.
    This way we can automatically generate documentation for all the
    monitors, including for the description field.

--- scalyr_agent/builtin_monitors/journald_monitor.py
+++ scalyr_agent/builtin_monitors/journald_monitor.py
@@ -257,8 +257,69 @@ class Checkpoint(object):
 
 
 class JournaldMonitor(ScalyrMonitor):
+    """
+# Journald Monitor
 
-    "Read logs from journalctl and emit to scalyr"
+A Scalyr agent monitor that imports log entries from journald.
+
+The journald monitor polls systemd journal files every ``journal_poll_interval`` seconds
+and uploads any new entries to the Scalyr servers.
+
+@class=bg-warning docInfoPanel: An *agent monitor plugin* is a component of the Scalyr Agent. To use a plugin,
+simply add it to the ``monitors`` section of the Scalyr Agent configuration file (``/etc/scalyr/agent.json``).
+For more information, see [Agent Plugins](/help/scalyr-agent#plugins).
+
+By default, the journald monitor logs all log entries, but it can also be configured to filter entries by specific
+fields.
+
+## Dependencies
+
+The journald monitor has a dependency on the Python systemd library, which is a Python wrapper around the systemd C API.
+You need to ensure this library has been installed on your system in order to use this monitor, otherwise a warning
+message will be printed if the Scalyr Agent is configured to use the journald monitor but the systemd library cannot be
+found.
+
+You can install the systemd Python library via package manager e.g.:
+
+    apt-get install python-systemd  (debian/ubuntu)
+    dnf install python-systemd  (CentOS/rhel/Fedora)
+
+Or install it from source using pip e.g.
+
+    pip install systemd-python
+
+See here for more information: https://github.com/systemd/python-systemd/
+
+## Sample Configuration
+
+The following example will configure the agent to query the journal entries
+located in /var/log/journal (the default location for persisted journald logs)
+
+    monitors: [
+      {
+        module: "scalyr_agent.builtin_monitors.journald_monitor",
+      }
+    ]
+
+Here is an example that queries journal entries from volatile/non-persisted journals, and filters those entries to only
+include ones that originate from the ssh service
+
+    monitors: [
+      {
+        module: "scalyr_agent.builtin_monitors.journald_monitor",
+        journal_path: "/run/log/journal",
+        journal_matches: ["_SYSTEMD_UNIT=ssh.service"]
+      }
+    ]
+
+## Polling the Journal File
+
+The journald monitor polls the journal file every ``journal_poll_interval`` seconds to check for new logs.  It does this by
+creating a polling object (https://docs.python.org/2/library/select.html#poll-objects) and calling the ``poll`` method
+of that object.  The ``poll`` method is called with a 0 second timeout so it never blocks.
+After processing any new events, or if there are no events to process, the monitor thread sleeps for ``journal_poll_interval``
+seconds and then polls again.
+    """
 
     def _initialize(self):
         verify_systemd_library_is_available()
--- scalyr_agent/builtin_monitors/mysql_monitor.py
+++ scalyr_agent/builtin_monitors/mysql_monitor.py
@@ -956,8 +956,59 @@ class MysqlDB(object):
 
 
 class MysqlMonitor(ScalyrMonitor):
-    """A Scalyr agent monitor that monitors mysql databases.
     """
+# MySQL Monitor
+
+This agent monitor plugin records performance and usage data from a MySQL server.
+
+NOTE: the MySQL monitor requires Python 2.6 or higher. (This applies to the server on which the Scalyr Agent
+is running, which needn't necessarily be the same machine where the MySQL server is running.) If you need
+to monitor MySQL from a machine running an older version of Python, [let us know](mailto:support@scalyr.com).
+
+@class=bg-warning docInfoPanel: An *agent monitor plugin* is a component of the Scalyr Agent. To use a plugin,
+simply add it to the ``monitors`` section of the Scalyr Agent configuration file (``/etc/scalyr/agent.json``).
+For more information, see [Agent Plugins](/help/scalyr-agent#plugins).
+
+## Sample Configuration
+
+To configure the MySQL monitor plugin, you will need the following information:
+
+- A MySQL username with administrative privileges. The user needs to be able to query the information_schema table,
+  as well as assorted global status information.
+- The password for that user.
+
+Here is a sample configuration fragment:
+
+    monitors: [
+      {
+         module:            "scalyr_agent.builtin_monitors.mysql_monitor",
+         database_socket:   "default",
+         database_username: "USERNAME",
+         database_password: "PASSWORD"
+      }
+    ]
+
+This configuration assumes that MySQL is running on the same server as the Scalyr Agent, and is using the default
+MySQL socket. If not, you will need to specify the server's socket file, or hostname (or IP address) and port number;
+see Configuration Reference.
+
+## Viewing Data
+
+After adding this plugin to the agent configuration file, wait one minute for data to begin recording. Then
+click the {{menuRef:Dashboards}} menu and select {{menuRef:MySQL}}. (The dashboard may not be listed until
+the agent begins sending MySQL data.) You will see an overview of MySQL performance statistics across all
+servers where you are running the MySQL plugin. Use the {{menuRef:ServerHost}} dropdown to show data for a
+specific server.
+
+The dashboard shows only some of the data collected by the MySQL monitor plugin. To explore the full range
+of data collected, go to the Search page and search for [$monitor = 'mysql_monitor'](/events?filter=$monitor%20%3D%20%27mysql_monitor%27).
+This will show all data collected by this plugin, across all servers. You can use the {{menuRef:Refine search by}}
+dropdown to narrow your search to specific servers and monitors.
+
+The [View Logs](/help/view) page describes the tools you can use to view and analyze log data.
+[Query Language](/help/query-language) lists the operators you can use to select specific metrics and values.
+You can also use this data in [Dashboards](/help/dashboards) and [Alerts](/help/alerts).
+"""
 
     def _initialize(self):
         """Performs monitor-specific initialization.
