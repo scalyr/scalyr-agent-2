c0a38b372491b8e7a20ba771b3578fc9df518baf
Tomaz Muraus <tomaz@tomaz.me>
2020-03-23 23:29:44 +0100

    Add additional test cases for shell monitor.

--- scalyr_agent/builtin_monitors/tests/shell_monitor_test.py
+++ scalyr_agent/builtin_monitors/tests/shell_monitor_test.py
@@ -61,3 +61,68 @@ class ShellMonitorTest(ScalyrTestCase):
         self.assertEqual(call_args, ("output", "foo..."))
         self.assertEqual(call_kwargs["extra_fields"]["command"], 'echo "foo bar!"')
         self.assertEqual(call_kwargs["extra_fields"]["length"], 9)
+
+    def test_gather_sample_log_all_lines(self):
+        monitor_config = {
+            "module": "shell_monitor",
+            "command": 'echo -e "line 1\nline 2\nline 3"',
+            "max_characters": 100,
+            "log_all_lines": True,
+        }
+        mock_logger = mock.Mock()
+        monitor = ShellMonitor(monitor_config, mock_logger)
+
+        monitor.gather_sample()
+        call_args_list = mock_logger.emit_value.call_args_list[0]
+        call_args = call_args_list[0]
+        call_kwargs = call_args_list[1]
+
+        self.assertEqual(call_args, ("output", "line 1\nline 2\nline 3\n"))
+        self.assertEqual(
+            call_kwargs["extra_fields"]["command"], 'echo -e "line 1\nline 2\nline 3"'
+        )
+        self.assertEqual(call_kwargs["extra_fields"]["length"], 21)
+
+    def test_gather_sample_extract_expression(self):
+        monitor_config = {
+            "module": "shell_monitor",
+            "command": 'echo -e "line 1\nline 2\nline 100\nline 200"',
+            "extract": r"line (\d\d\d)",
+            "max_characters": 100,
+        }
+        mock_logger = mock.Mock()
+        monitor = ShellMonitor(monitor_config, mock_logger)
+
+        monitor.gather_sample()
+        call_args_list = mock_logger.emit_value.call_args_list[0]
+        call_args = call_args_list[0]
+        call_kwargs = call_args_list[1]
+
+        self.assertEqual(call_args, ("output", "100"))
+        self.assertEqual(
+            call_kwargs["extra_fields"]["command"],
+            'echo -e "line 1\nline 2\nline 100\nline 200"',
+        )
+        self.assertEqual(call_kwargs["extra_fields"]["length"], 3)
+
+    def test_gather_sample_stdout_and_stderr_is_combined(self):
+        monitor_config = {
+            "module": "shell_monitor",
+            "command": 'echo "stdout" ; >&2 echo "stderr"',
+            "extract": r"line (\d\d\d)",
+            "max_characters": 100,
+            "log_all_lines": True,
+        }
+        mock_logger = mock.Mock()
+        monitor = ShellMonitor(monitor_config, mock_logger)
+
+        monitor.gather_sample()
+        call_args_list = mock_logger.emit_value.call_args_list[0]
+        call_args = call_args_list[0]
+        call_kwargs = call_args_list[1]
+
+        self.assertEqual(call_args, ("output", "stderr\n\nstdout\n"))
+        self.assertEqual(
+            call_kwargs["extra_fields"]["command"], 'echo "stdout" ; >&2 echo "stderr"',
+        )
+        self.assertEqual(call_kwargs["extra_fields"]["length"], 15)
