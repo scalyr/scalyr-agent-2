6ceb3d6c70034b6567d9f9206a5804005a0c4443
Tomaz Muraus <tomaz@tomaz.me>
2020-04-09 21:42:41 +0200

    Make sure data.log file exists before starting the agent, otherwise it won't be consumed after starting it.
    I believe this should fix the failures we are seeing on CI.
    
    I believe this never worked correctly. Previously it worked purely at
    random / out of luck, because we incorrectly cached data.log file and
    new runs simply re-used old and cached (already existing data.log file).
    
    This all broken when the cache was invalided / purged.

--- tests/smoke_tests/common.py
+++ tests/smoke_tests/common.py
@@ -18,6 +18,9 @@ from __future__ import absolute_import
 
 import os
 import time
+from io import open
+
+import six
 
 from scalyr_agent import compat
 from scalyr_agent.__scalyr__ import DEV_INSTALL, get_package_root
@@ -56,6 +59,19 @@ def _test_standalone_smoke(agent_installation_type, python_version=None):
     process_metrics_verifier = ProcessMetricsVerifier(
         runner, compat.os_environ_unicode["SCALYR_SERVER"]
     )
+
+    # NOTE: It's important that the file exists before starting the agent otherwise it won't be
+    # consumed by the agent and the tests will fail.
+    agent_logs_dir_path = six.text_type(runner.agent_logs_dir_path)
+    data_log_path = six.text_type(data_json_verifier._data_json_log_path)
+
+    if os.path.exists(data_log_path):
+        os.utime(data_log_path)
+    else:
+        os.makedirs(agent_logs_dir_path)
+
+        with open(data_log_path, "a") as _:
+            pass
 
     runner.start()
 
