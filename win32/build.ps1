afe9fa58d488445f56a39b46013b1c9ae26f4633
Arthur Kamalov <artur.kamalov@akvelon.com>
2020-05-05 12:13:03 +0300

    build windows package in CircleCI

--- win32/build.ps1
+++ win32/build.ps1
@@ -1,5 +1,5 @@
-#wget https://github.com/wixtoolset/wix3/releases/download/wix3112rtm/wix311-binaries.zip -OutFile wix311-binaries.zip
-#Expand-Archive -LiteralPath wix311-binaries.zip -DestinationPath C:\wix311
+wget https://github.com/wixtoolset/wix3/releases/download/wix3112rtm/wix311-binaries.zip -OutFile wix311-binaries.zip
+Expand-Archive -LiteralPath wix311-binaries.zip -DestinationPath C:\wix311
 $Env:Path += ";C:\wix311"
 $Env:WIX = "C:\wix311"
 
