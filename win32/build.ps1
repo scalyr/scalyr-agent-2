c28bc6f10ddc4cf6a9ed7446f0d70a4e65a2c077
Arthur Kamalov <artur.kamalov@akvelon.com>
2020-05-05 11:55:59 +0300

    build windows package in CircleCI

---
+++ win32/build.ps1
@@ -0,0 +1,7 @@
+#wget https://github.com/wixtoolset/wix3/releases/download/wix3112rtm/wix311-binaries.zip -OutFile wix311-binaries.zip
+#Expand-Archive -LiteralPath wix311-binaries.zip -DestinationPath C:\wix311
+$Env:Path += ";C:\wix311"
+$Env:WIX = "C:\wix311"
+
+C:\Python37\python.exe -m pip install psutil
+C:\Python37\python.exe build_package.py win32
