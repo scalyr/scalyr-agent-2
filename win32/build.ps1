f8201ebdf0d22e444d41de7a9c4e61a423c7a609
Arthur Kamalov <artur.kamalov@akvelon.com>
2020-05-05 12:25:29 +0300

    Change str to six.text_type in apache monitor.

--- win32/build.ps1
+++ win32/build.ps1
@@ -1,7 +1,9 @@
+$python_dir_path=$args[0]
+
 wget https://github.com/wixtoolset/wix3/releases/download/wix3112rtm/wix311-binaries.zip -OutFile wix311-binaries.zip
 Expand-Archive -LiteralPath wix311-binaries.zip -DestinationPath C:\wix311
 $Env:Path += ";C:\wix311"
 $Env:WIX = "C:\wix311"
 
-C:\Python37\python.exe -m pip install psutil
-C:\Python37\python.exe build_package.py win32
+& "${python_dir_path}\python.exe" -m pip install psutil
+& "${python_dir_path}\python.exe" build_package.py win32
