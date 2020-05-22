372f2e3522728bec55ceb1778be6f05caa8e4ced
Arthur Kamalov <artur.kamalov@akvelon.com>
2020-05-22 15:16:16 +0300

    add script to deploy ssh server on windows.

---
+++ tests/ami/windows/install_openssh.ps1
@@ -0,0 +1,28 @@
+$public_key=$args[0]
+
+wget https://github.com/PowerShell/Win32-OpenSSH/releases/download/v8.1.0.0p1-Beta/OpenSSH-Win64.zip -OutFile OpenSSH-Win64.zip;
+Expand-Archive -LiteralPath OpenSSH-Win64.zip -DestinationPath "C:\Program Files" -Force;
+mv "C:\Program Files\OpenSSH-Win64" "C:\Program Files\openssh";
+$env:Path += "C:\Program Files\openssh";
+
+& "C:\Program Files\openssh\install-sshd.ps1";
+
+New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22;
+
+
+Set-Service sshd -StartupType Automatic;
+net start sshd;
+
+New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force;
+New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShellCommandOption -Value "/c" -PropertyType String -Force;
+
+mkdir ~\.ssh -Force;
+Add-Content ~\.ssh\authorized_keys $public_key;
+
+(Get-Content "C:\ProgramData\ssh\sshd_config") `
+   -replace 'Match Group administrators', '#Match Group administrators' `
+   -replace "       AuthorizedKeysFile __PROGRAMDATA__/ssh/administrators_authorized_keys", "#       AuthorizedKeysFile __PROGRAMDATA__/ssh/administrators_authorized_keys" |
+ Out-File "C:\ProgramData\ssh\sshd_config" -Encoding utf8;
+
+net stop sshd;
+net start sshd;
