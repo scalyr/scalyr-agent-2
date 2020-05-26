6ad03f8cb9be434fb61bfae92451707887131042
yanscalyr <yan@scalyr.com>
2020-05-26 15:14:10 -0700

    Continue trying to narrow down problematic config option

--- tests/utils/agent_runner.py
+++ tests/utils/agent_runner.py
@@ -350,7 +350,7 @@ class AgentRunner(object):
             "server_attributes": {"serverHost": self._server_host},
             "logs": list(self._log_files.values()),
             "monitors": [],
-            "max_log_offset_size": 5242880,
+            "pipeline_threshold": 1.1,
         }
 
         if self._enable_debug_log:
