c8fccd81c4f2dc3f0db4413c1afe919b47b25ba1
yanscalyr <yan@scalyr.com>
2020-06-02 11:41:48 -0700

    Better override values

--- scalyr_agent/configuration.py
+++ scalyr_agent/configuration.py
@@ -249,8 +249,8 @@ class Configuration(object):
 
                 self.__config["max_allowed_request_size"] = 5900000
                 self.__config["pipeline_threshold"] = 0
-                self.__config["min_request_spacing_interval"] = 0.1
-                self.__config["max_request_spacing_interval"] = 1.0
+                self.__config["min_request_spacing_interval"] = 0.0
+                self.__config["max_request_spacing_interval"] = 5.0
                 self.__config["max_log_offset_size"] = 200000000
                 self.__config["max_existing_log_offset_size"] = 200000000
 
