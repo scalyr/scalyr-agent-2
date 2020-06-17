4c17f8ba5023a697d0b97952c7668d652f4eb62f
Tomaz Muraus <tomaz@tomaz.me>
2020-06-17 18:04:05 +0200

    Add WIP test job which sends benchmark results graphs to Slack.

---
+++ scripts/phantomjs/capture_codespeed_graphs_screenshots.js
@@ -0,0 +1,63 @@
+/**
+#  Copyright 2014 Scalyr Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+**/
+
+// Script which captures screenshot of the CodeSpeed timeline page for a specific set of benchmarks
+// and stores it to disk.
+var webpage = require('webpage');
+var system = require('system');
+
+var CODESPEED_AUTH = system.env['CODESPEED_AUTH'];
+
+// How much time to wait for chart XHR data to load
+var CHART_LOAD_TIMEOUT = 5000;
+
+var TIMELIME_CHARTS_TO_CAPTURE_URLS = [
+  'https://' + CODESPEED_AUTH + '@scalyr-agent-codespeed.herokuapp.com/timeline/?exe=2%2C6%2C3%2C7%2C10%2C1%2C5%2C4%2C8&base=1%2B95&ben=memory_usage_rss&env=1&revs=30&equid=off&quarts=on&extr=on',
+  'https://' + CODESPEED_AUTH + '@scalyr-agent-codespeed.herokuapp.com/timeline/?exe=2%2C6%2C3%2C7%2C10%2C1%2C5%2C4%2C8&base=1%2B95&ben=log_lines_error&env=1&revs=30&equid=off&quarts=on&extr=on',
+  'https://' + CODESPEED_AUTH + '@scalyr-agent-codespeed.herokuapp.com/timeline/?exe=2%2C6%2C3%2C7%2C10%2C1%2C5%2C4%2C8&base=1%2B95&ben=log_lines_info&env=1&revs=30&equid=off&quarts=on&extr=on',
+  'https://' + CODESPEED_AUTH + '@scalyr-agent-codespeed.herokuapp.com/timeline/?exe=2%2C6%2C3%2C7%2C10%2C1%2C5%2C4%2C8&base=1%2B95&ben=log_lines_debug&env=1&revs=30&equid=off&quarts=on&extr=on',
+
+]
+
+var DESTINATION_PATHS = [
+  '/tmp/codespeed-memory_usage_rss.png',
+  '/tmp/codespeed-log_lines_error.png',
+  '/tmp/codespeed-log_lines_info.png',
+  '/tmp/codespeed-log_lines_debug.png',
+]
+
+function capture_screenshot(url, destination_path) {
+  console.log("Capturing screenshot for " + url);
+
+  var page = webpage.create();
+  page.viewportSize = { width: 1680, height: 768 };
+  page.clipRect = { top: 0, left: 0, width: 1680, height: 768 };
+  page.open(url, function() {
+    setTimeout(function() {
+      page.render(destination_path);
+      console.log('Screenshot saved to ' + destination_path);
+    }, CHART_LOAD_TIMEOUT);
+  });
+}
+
+for (var i = 0; i < TIMELIME_CHARTS_TO_CAPTURE_URLS.length; i++) {
+  var url = TIMELIME_CHARTS_TO_CAPTURE_URLS[i];
+  var destination_path = DESTINATION_PATHS[i];
+
+  capture_screenshot(url, destination_path);
+}
+
+setTimeout(phantom.exit, 10 * 1000);
