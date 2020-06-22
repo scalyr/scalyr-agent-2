2df82a20d30e1fe87dde49a5806c06e716191c22
Arthur Kamalov <artur.kamalov@akvelon.com>
2020-06-23 00:15:36 +0300

    fix

--- scripts/circleci/upload_circleci_artifacts.py
+++ scripts/circleci/upload_circleci_artifacts.py
@@ -180,6 +180,8 @@ def wait_for_workflow(workflow_id, timeout_time):
         workflow_status = workflow_info["status"]
         workflow_name = workflow_info["name"]
 
+        print("Status: ", workflow_name)
+
         if workflow_status == "success":
             print("Workflow '{0}' finished successfully.".format(workflow_name))
             return workflow_info
