1340b89382b6580c6ad611ba031cc354882a95f5
yanscalyr <yan@scalyr.com>
2020-07-01 14:31:13 -0700

    Testing

--- tests/smoke_tests/verifier.py
+++ tests/smoke_tests/verifier.py
@@ -454,11 +454,11 @@ class DataJsonVerifierRateLimited(AgentVerifier):
         }
 
         self._lines_count = 5000
-        self._upload_wait_time = 20
+        self._upload_wait_time = 30
         # Estimate of line size
         self._line_size = len(json.dumps(self._message))
         self._expected_lines_uploaded = (
-            1000000
+            500000
             * (
                 self._upload_wait_time + 4
             )  # 200000 is the rate in bytes we configured in the config
