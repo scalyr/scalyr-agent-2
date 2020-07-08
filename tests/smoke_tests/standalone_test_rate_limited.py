467ec4b7732c17189820f6b36c2485cc75e45734
yanscalyr <57232095+yanscalyr@users.noreply.github.com>
2020-07-08 09:55:09 -0700

    End-to-end test for rate limiting functionality (#587)
    * Test run to see if it picks up the new test at all
    
    * Test run to see if it picks up the new test at all
    
    * Test run to see if it picks up the new test at all
    
    * Remove unneeded code
    
    * Logic error and naming error
    
    * Actually generate lines
    
    * Test with more time to see if anything reaches the server in time
    
    * Testing
    
    * Testing
    
    * Add ingestion delay
    
    * Test with rate limit
    
    * Test with rate limit
    
    * Test with rate limit
    
    * Try changing timestamps
    
    * Try with smaller max request size
    
    * Try with smaller max request size
    
    * Try with smaller max request size
    
    * Send more data with a larger max request size
    
    * Fiddle with values
    
    * Fiddle with values
    
    * Fiddle with values
    
    * Fiddle with values
    
    * Output found and expected on success
    
    * Try a longer test
    
    * Add in the bucket size
    
    * Try larger message sizes
    
    * Try smaller message sizes
    
    * Try even larger message sizes
    
    * Bigger message and longer wait time
    
    * Increase time further
    
    * Increase rate further
    
    * 5000 is out line limit
    
    * Try higher message size again
    
    * Try default message size just to see
    
    * Try without the bucket size
    
    * aaaah
    
    * Try smaller message
    
    * Set min request spacing interval to 0.0
    
    * Testing
    
    * Testing
    
    * Testing
    
    * Testing
    
    * Testing
    
    * Testing
    
    * Testing
    
    * Testing
    
    * Testing
    
    * Testing
    
    * Testing
    
    * Testing
    
    * Testing
    
    * Testing
    
    * Testing
    
    * Testing
    
    * Testing
    
    * Testing
    
    * Testing
    
    * Turn back on all tests
    
    * Add comments
    
    * Name the filler better
    
    * Add some additional logging which may help to troubleshoot the failures.
    
    * Update DataJsonVerifierRateLimited so we also try to retry actual
    queries which verify if data has been ingested.
    
    This is is needed since it can take a while for data to be ingested and
    similar.
    
    * Make the code easier to read, fix typo.
    
    * Use smaller max time - if there is nothing wrong, those tests should
    finish faster.
    
    * Test a change. Sadly there is no other way way to get that file to the
    container without rebuilding the base image.
    
    * Download smoketests.py script from a corresponding git branch.
    
    * Use smaller max wait / timeout for various moke tests (150 instead of
    300 seconds).
    
    If everything is right, those tests finish much earlier than in 150
    seconds.
    
    And if something is wrong and test would fail because of that, we want
    to fail earlier instead of waiting 300 seconds which is not needed.
    
    * Simplify the code and use one line less.
    
    * Don't assert on the attribute which is not present anymore.
    
    * Partial review response
    
    Co-authored-by: Tomaz Muraus <tomaz@tomaz.me>

---
+++ tests/smoke_tests/standalone_test_rate_limited.py
@@ -0,0 +1,28 @@
+# Copyright 2014-2020 Scalyr Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import absolute_import
+
+import pytest
+
+from scalyr_agent.__scalyr__ import DEV_INSTALL
+from tests.smoke_tests.common import _test_standalone_smoke
+
+
+@pytest.mark.usefixtures("agent_environment")
+@pytest.mark.timeout(300)
+def test_standalone_smoke():
+    _test_standalone_smoke(DEV_INSTALL, rate_limited=True)
