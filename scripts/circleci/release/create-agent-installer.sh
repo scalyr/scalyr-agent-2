7cbf4b66ccfd4c2bc5a7e4d0282007a07be2932a
Arthur Kamalov <artur.kamalov@akvelon.com>
2020-12-27 23:36:04 +0300

    remove comment with the placeholder in the install script.

--- scripts/circleci/release/create-agent-installer.sh
+++ scripts/circleci/release/create-agent-installer.sh
@@ -360,7 +360,7 @@ tar -cf repo_packages.tar *bootstrap*.rpm *bootstrap*.deb
 
 # replace a special placeholder for the repository type in the install sript to determine a final URL of the repository.
 # The comment that follows after the placeholder is also replaced.
-sed sed "s~{ % REPLACE_REPOSITORY_TYPE % }\" #// .*//~$REPO_BASE_URL~g" $SCRIPTPATH/installScalyrAgentV2.sh > installScalyrAgentV2.sh
+sed sed "s~{ % REPLACE_REPOSITORY_TYPE % }\" #.*//~$REPO_BASE_URL~g" $SCRIPTPATH/installScalyrAgentV2.sh > installScalyrAgentV2.sh
 
 
 cat repo_packages.tar >> installScalyrAgentV2.sh
