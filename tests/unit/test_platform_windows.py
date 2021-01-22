73c78c2856ddd17afa9afef521121ff5e9f87476
Tomaz Muraus <tomaz@tomaz.me>
2021-01-22 11:26:31 +0100

    Use lazy import to avoid cross test pollution + add a workaround.

--- tests/unit/test_platform_windows.py
+++ tests/unit/test_platform_windows.py
@@ -22,28 +22,32 @@ from scalyr_agent.test_base import ScalyrTestCase
 from scalyr_agent.test_base import skipIf
 
 if platform.system() == "Windows":
-    from scalyr_agent.platform_windows import WindowsPlatformController
+    WINDOWS = True
 else:
-    WindowsPlatformController = None  # type: ignore
+    WINDOWS = False
 
 
 class WindowsPlatformControllerTestCase(ScalyrTestCase):
-    @skipIf(not WindowsPlatformController, "Skipping tests under non-Windows platform")
+    @skipIf(not WINDOWS, "Skipping tests under non-Windows platform")
     @mock.patch("scalyr_agent.platform_windows._set_config_path_registry_entry")
     def test_start_agent_service_friendly_error_on_insufficient_permissions(
         self, mock_set_config_path_registry_entry
     ):
+        from scalyr_agent.platform_windows import WindowsPlatformController
+
         mock_set_config_path_registry_entry.side_effect = Exception("Access is denied")
 
         controller = WindowsPlatformController()
         expected_msg = r".*Unable to set registry entry.*"
         self.assertRaisesRegexp(Exception, expected_msg, controller.start_agent_service)
 
-    @skipIf(not WindowsPlatformController, "Skipping tests under non-Windows platform")
+    @skipIf(not WINDOWS, "Skipping tests under non-Windows platform")
     @mock.patch("win32serviceutil.StopService")
     def test_stop_agent_service_friendly_error_on_insufficient_permissions(
         self, mock_StopService
     ):
+        from scalyr_agent.platform_windows import WindowsPlatformController
+
         mock_StopService.side_effect = Exception("Access is denied")
         controller = WindowsPlatformController()
         expected_msg = r".*Unable to stop agent process.*"
