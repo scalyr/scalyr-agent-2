7e07abe83243accb6b055adf99c2189215242c4f
Arthur Kamalov <artur.kamalov@akvelon.com>
2021-01-22 04:15:21 +0300

    add check

--- scalyr_agent/builtin_monitors/windows_event_log_monitor.py
+++ scalyr_agent/builtin_monitors/windows_event_log_monitor.py
@@ -452,13 +452,14 @@ class NewApi(Api):
         Close all handles for event subscriptions.
         """
         for handle in self.__eventHandles:
-            res = windll.wevtapi.EvtClose(handle.handle)
-            if not res:
-                self._logger.error(
-                    "Can not close event subscription handle '{0}'.".format(
-                        handle.handle
-                    )
-                )
+            pass
+            # res = windll.wevtapi.EvtClose(handle.handle)
+            # if not res:
+            #     self._logger.error(
+            #         "Can not close event subscription handle '{0}'.".format(
+            #             handle.handle
+            #         )
+            #     )
         self.__eventHandles = []
 
     def _FormattedMessage(self, metadata, event, field, value):
