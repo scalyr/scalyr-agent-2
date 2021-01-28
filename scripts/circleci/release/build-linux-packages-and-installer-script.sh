0089e7e54aa59daa81278623d8b1126049fb0132
Arthur Kamalov <artur.kamalov@akvelon.com>
2021-01-28 21:01:14 +0300

    small fixes

--- scripts/circleci/release/build-linux-packages-and-installer-script.sh
+++ scripts/circleci/release/build-linux-packages-and-installer-script.sh
@@ -1,3 +1,31 @@
+#!/usr/bin/env bash
+#
+# Copyright 2014-2021 Scalyr Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# =========================================================================
+#
+# This script is used to create build linux based packages agent, install script and other related things.
+# For now it is supposed to work only within the circleci job.
+# Usage: build-linux-packages-and-installer-script.sh <AGENT_RELEASE_VERSION> <OUTPUT_PATH> <RELEASE_REPO_BASE_URL> <RELEASE_REPO_NAME>
+#
+# AGENT_RELEASE_VERSION - the version for the agent to set. The version form the VERSION file is used if emoty.
+# OUTPUT_PATH - the path for all build artifacts.
+# RELEASE_REPO_BASE_URL - the base url for the different types of release S3 repos (stable, beta, internal).
+# RELEASE_REPO_NAME - the type of the S3 repo (stable, beta, internal).
+
+set -e
 
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 AGENT_SOURCE_PATH="$(realpath "${SCRIPTPATH}/../../..")"
@@ -7,8 +35,6 @@ AGENT_RELEASE_VERSION=$1
 OUTPUT_PATH="$(realpath "$2")"
 RELEASE_REPO_BASE_URL=${3:-stable}
 RELEASE_REPO_NAME=${4:-stable}
-
-set -e
 
 VERSION_FILE_PATH="${AGENT_SOURCE_PATH}/VERSION"
 
@@ -61,6 +87,5 @@ echo "Build deb and rpm repo packages and installer script."
 bash ${AGENT_SOURCE_PATH}/scripts/circleci/release/create-agent-installer.sh "$GPG_SIGNING_KEYID" "$GPG_ALT_SIGNING_KEYID" "$RELEASE_REPO_BASE_URL" "$RELEASE_REPO_NAME"
 
 cat "${VERSION_FILE_PATH}" > RELEASE_VERSION
-
 
 popd
