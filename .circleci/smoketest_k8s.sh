e4c7867cb1db6cd50cc6881199cd393d58f611bf
yan <yan@scalyr.com>
2021-03-18 11:37:05 -0700

    Try wget hack to avoid rebuilding smoketests

--- .circleci/smoketest_k8s.sh
+++ .circleci/smoketest_k8s.sh
@@ -16,8 +16,6 @@
 #   SCALYR_SERVER
 #   READ_API_KEY (Read api key. 'SCALYR_' prefix intentionally omitted to suppress in status -v)
 #   CIRCLE_BUILD_NUM
-#   SMOKE_TESTS_SCRIPT_REPO
-#   SMOKE_TESTS_SCRIPT_BRANCH
 #
 # Expects following positional args:
 #   $1 : smoketest image tag
@@ -37,6 +35,14 @@ max_wait=$2
 
 # Flag indicating whether to delete pre-existing k8s objects
 delete_existing_objects=$3
+
+# We don't have an easy way to update base test docker images which come bundled
+# with the smoketest.py file
+# (.circleci/docker_unified_smoke_unit/smoketest/smoketest.py ->
+# /tmp/smoketest.py) so we simply download this file from the github before running the tests.
+# That's not great, but it works.
+SMOKE_TESTS_SCRIPT_BRANCH=${CIRCLE_BRANCH:-"master"}
+SMOKE_TESTS_SCRIPT_REPO=${CIRCLE_PROJECT_REPONAME:-"scalyr-agent-2"}
 
 # Smoketest code
 SMOKE_TESTS_SCRIPT_URL="https://raw.githubusercontent.com/scalyr/${SMOKE_TESTS_SCRIPT_REPO}/${SMOKE_TESTS_SCRIPT_BRANCH}/.circleci/docker_unified_smoke_unit/smoketest/smoketest.py"
