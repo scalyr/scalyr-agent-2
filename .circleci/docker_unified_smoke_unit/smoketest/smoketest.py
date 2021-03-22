ad377d73bbad848b85429e26e17a83f74db15ff7
yan <yan@scalyr.com>
2021-03-21 23:40:56 -0700

    Update test to check for correct container name

--- .circleci/docker_unified_smoke_unit/smoketest/smoketest.py
+++ .circleci/docker_unified_smoke_unit/smoketest/smoketest.py
@@ -1026,7 +1026,8 @@ class K8sActor(DockerSmokeTestActor):
                 att.get("stream") in stream_name,
                 att.get("monitor") == "agentKubernetes",
                 process_name in att.get("pod_name"),
-                att.get("container_name") == "scalyr-agent",
+                "ci-agent-k8s" in att.get("container_name"),
+                "uploader" in att.get("container_name"),
             ]
         ):
             return False
