6950244972e5b6850831f48b4d0e11a080289421
yan <yan@scalyr.com>
2021-03-21 23:05:17 -0700

    Missed a spot

--- .circleci/smoketest_k8s.sh
+++ .circleci/smoketest_k8s.sh
@@ -131,7 +131,7 @@ cp .circleci/scalyr-agent-2-with-extra-config.yaml .
 perl -pi.bak -e 's/image\:\s+(\S+)/image: local_k8s_image/' scalyr-agent-2.yaml
 perl -pi.bak -e 's/imagePullPolicy\:\s+(\S+)/imagePullPolicy: Never/' scalyr-agent-2.yaml
 
-kubectl create -f ./scalyr-agent-2.yaml
+kubectl create -f ./scalyr-agent-2-with-extra-config.yaml
 # Capture agent pod
 agent_hostname=$(kubectl get pods | fgrep scalyr-agent-2 | awk {'print $1'})
 echo "Agent pod == ${agent_hostname}"
