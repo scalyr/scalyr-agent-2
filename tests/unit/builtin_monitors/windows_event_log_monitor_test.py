7802f31dbbdd96e2745c4889e2622376c106f6a1
Tomaz Muraus <tomazm@sentinelone.com>
2021-08-11 15:16:08 +0200

    Fix lint.

--- tests/unit/builtin_monitors/windows_event_log_monitor_test.py
+++ tests/unit/builtin_monitors/windows_event_log_monitor_test.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import absolute_import
+
 import mock
 
 import scalyr_agent.builtin_monitors.windows_event_log_monitor
