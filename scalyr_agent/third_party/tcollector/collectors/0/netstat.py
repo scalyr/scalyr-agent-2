36c2679748a720babf41c6b328047bda95c9577e
Tomaz Muraus <tomazm@sentinelone.com>
2021-11-12 19:40:56 +0100

    Use OrderedDict to ensure consistent and stable output order.

--- scalyr_agent/third_party/tcollector/collectors/0/netstat.py
+++ scalyr_agent/third_party/tcollector/collectors/0/netstat.py
@@ -68,6 +68,8 @@ import sys
 import time
 from io import open
 
+from collections import OrderedDict
+
 # If we're running as root and this user exists, we'll drop privileges.
 USER = "nobody"
 
@@ -142,118 +144,118 @@ def parse_and_print_metrics(f_netstat, f_sockstat, output_file_success=None,
 
     # If a line in /proc/net/netstat doesn't start with a word in that dict,
     # we'll ignore it.  We use the value to build the metric name.
-    known_netstatstypes = {
-        "TcpExt:": "tcp",
-        "IpExt:": "ip",  # We don't collect anything from here for now.
-        "Ip:": "ip",  # We don't collect anything from here for now.
-        "Icmp:": "icmp",  # We don't collect anything from here for now.
-        "IcmpMsg:": "icmpmsg",  # We don't collect anything from here for now.
-        "Tcp:": "tcp",  # We don't collect anything from here for now.
-        "Udp:": "udp",
-        "UdpLite:": "udplite",  # We don't collect anything from here for now.
-        "Arista:": "arista",  # We don't collect anything from here for now.
-    }
+    known_netstatstypes = OrderedDict([
+        ("TcpExt:", "tcp"),
+        ("IpExt:", "ip"),  # We don't collect anything from here for now.
+        ("Ip:", "ip"),  # We don't collect anything from here for now.
+        ("Icmp:", "icmp"),  # We don't collect anything from here for now.
+        ("IcmpMsg:", "icmpmsg"),  # We don't collect anything from here for now.
+        ("Tcp:", "tcp"),  # We don't collect anything from here for now.
+        ("Udp:", "udp"),
+        ("UdpLite:", "udplite"),  # We don't collect anything from here for now.
+        ("Arista:", "arista"),  # We don't collect anything from here for now.
+    ])
 
     # Any stat in /proc/net/netstat that doesn't appear in this dict will be
     # ignored.  If we find a match, we'll use the (metricname, tags).
-    known_netstats = {
+    known_netstats = OrderedDict([
         # An application wasn't able to accept a connection fast enough, so
         # the kernel couldn't store an entry in the queue for this connection.
         # Instead of dropping it, it sent a cookie to the client.
-        "SyncookiesSent": ("syncookies", "type=sent"),
+        ("SyncookiesSent", ("syncookies", "type=sent")),
         # After sending a cookie, it came back to us and passed the check.
-        "SyncookiesRecv": ("syncookies", "type=received"),
+        ("SyncookiesRecv", ("syncookies", "type=received")),
         # After sending a cookie, it came back to us but looked invalid.
-        "SyncookiesFailed": ("syncookies", "type=failed"),
+        ("SyncookiesFailed", ("syncookies", "type=failed")),
         # When a socket is using too much memory (rmem), the kernel will first
         # discard any out-of-order packet that has been queued (with SACK).
-        "OfoPruned": ("memory.prune", "type=drop_ofo_queue"),
+        ("OfoPruned", ("memory.prune", "type=drop_ofo_queue")),
         # If the kernel is really really desperate and cannot give more memory
         # to this socket even after dropping the ofo queue, it will simply
         # discard the packet it received.  This is Really Bad.
-        "RcvPruned": ("memory.prune", "type=drop_received"),
+        ("RcvPruned", ("memory.prune", "type=drop_received")),
         # We waited for another packet to send an ACK, but didn't see any, so
         # a timer ended up sending a delayed ACK.
-        "DelayedACKs": ("delayedack", "type=sent"),
+        ("DelayedACKs", ("delayedack", "type=sent")),
         # We wanted to send a delayed ACK but failed because the socket was
         # locked.  So the timer was reset.
-        "DelayedACKLocked": ("delayedack", "type=locked"),
+        ("DelayedACKLocked", ("delayedack", "type=locked")),
         # We sent a delayed and duplicated ACK because the remote peer
         # retransmitted a packet, thinking that it didn't get to us.
-        "DelayedACKLost": ("delayedack", "type=lost"),
+        ("DelayedACKLost", ("delayedack", "type=lost")),
         # We completed a 3WHS but couldn't put the socket on the accept queue,
         # so we had to discard the connection.
-        "ListenOverflows": ("failed_accept", "reason=full_acceptq"),
+        ("ListenOverflows", ("failed_accept", "reason=full_acceptq")),
         # We couldn't accept a connection because one of: we had no route to
         # the destination, we failed to allocate a socket, we failed to
         # allocate a new local port bind bucket.  Note: this counter
         # also include all the increments made to ListenOverflows...
-        "ListenDrops": ("failed_accept", "reason=other"),
+        ("ListenDrops", ("failed_accept", "reason=other")),
         # A packet was lost and we recovered after a fast retransmit.
-        "TCPRenoRecovery": ("packetloss.recovery", "type=fast_retransmit"),
+        ("TCPRenoRecovery", ("packetloss.recovery", "type=fast_retransmit")),
         # A packet was lost and we recovered by using selective
         # acknowledgements.
-        "TCPSackRecovery": ("packetloss.recovery", "type=sack"),
+        ("TCPSackRecovery", ("packetloss.recovery", "type=sack")),
         # We detected re-ordering using FACK (Forward ACK -- the highest
         # sequence number known to have been received by the peer when using
         # SACK -- FACK is used during congestion control).
-        "TCPFACKReorder": ("reording", "detectedby=fack"),
+        ("TCPFACKReorder", ("reording", "detectedby=fack")),
         # We detected re-ordering using SACK.
-        "TCPSACKReorder": ("reording", "detectedby=sack"),
+        ("TCPSACKReorder", ("reording", "detectedby=sack")),
         # We detected re-ordering using fast retransmit.
-        "TCPRenoReorder": ("reording", "detectedby=fast_retransmit"),
+        ("TCPRenoReorder", ("reording", "detectedby=fast_retransmit")),
         # We detected re-ordering using the timestamp option.
-        "TCPTSReorder": ("reording", "detectedby=timestamp"),
+        ("TCPTSReorder", ("reording", "detectedby=timestamp")),
         # We detected some erroneous retransmits and undid our CWND reduction.
-        "TCPFullUndo": ("congestion.recovery", "type=full_undo"),
+        ("TCPFullUndo", ("congestion.recovery", "type=full_undo")),
         # We detected some erroneous retransmits, a partial ACK arrived while
         # we were fast retransmitting, so we were able to partially undo some
         # of our CWND reduction.
-        "TCPPartialUndo": ("congestion.recovery", "type=hoe_heuristic"),
+        ("TCPPartialUndo", ("congestion.recovery", "type=hoe_heuristic")),
         # We detected some erroneous retransmits, a D-SACK arrived and ACK'ed
         # all the retransmitted data, so we undid our CWND reduction.
-        "TCPDSACKUndo": ("congestion.recovery", "type=sack"),
+        ("TCPDSACKUndo", ("congestion.recovery", "type=sack")),
         # We detected some erroneous retransmits, a partial ACK arrived, so we
         # undid our CWND reduction.
-        "TCPLossUndo": ("congestion.recovery", "type=ack"),
+        ("TCPLossUndo", ("congestion.recovery", "type=ack")),
         # We received an unexpected SYN so we sent a RST to the peer.
-        "TCPAbortOnSyn": ("abort", "type=unexpected_syn"),
+        ("TCPAbortOnSyn", ("abort", "type=unexpected_syn")),
         # We were in FIN_WAIT1 yet we received a data packet with a sequence
         # number that's beyond the last one for this connection, so we RST'ed.
-        "TCPAbortOnData": ("abort", "type=data_after_fin_wait1"),
+        ("TCPAbortOnData", ("abort", "type=data_after_fin_wait1")),
         # We received data but the user has closed the socket, so we have no
         # wait of handing it to them, so we RST'ed.
-        "TCPAbortOnClose": ("abort", "type=data_after_close"),
+        ("TCPAbortOnClose", ("abort", "type=data_after_close")),
         # This is Really Bad.  It happens when there are too many orphaned
         # sockets (not attached a FD) and the kernel has to drop a connection.
         # Sometimes it will send a reset to the peer, sometimes it wont.
-        "TCPAbortOnMemory": ("abort", "type=out_of_memory"),
+        ("TCPAbortOnMemory", ("abort", "type=out_of_memory")),
         # The connection timed out really hard.
-        "TCPAbortOnTimeout": ("abort", "type=timeout"),
+        ("TCPAbortOnTimeout", ("abort", "type=timeout")),
         # We killed a socket that was closed by the application and lingered
         # around for long enough.
-        "TCPAbortOnLinger": ("abort", "type=linger"),
+        ("TCPAbortOnLinger", ("abort", "type=linger")),
         # We tried to send a reset, probably during one of teh TCPABort*
         # situations above, but we failed e.g. because we couldn't allocate
         # enough memory (very bad).
-        "TCPAbortFailed": ("abort.failed", None),
+        ("TCPAbortFailed", ("abort.failed", None)),
         # Number of times a socket was put in "memory pressure" due to a non
         # fatal memory allocation failure (reduces the send buffer size etc).
-        "TCPMemoryPressures": ("memory.pressure", None),
+        ("TCPMemoryPressures", ("memory.pressure", None)),
         # We got a completely invalid SACK block and discarded it.
-        "TCPSACKDiscard": ("invalid_sack", "type=invalid"),
+        ("TCPSACKDiscard", ("invalid_sack", "type=invalid")),
         # We got a duplicate SACK while retransmitting so we discarded it.
-        "TCPDSACKIgnoredOld": ("invalid_sack", "type=retransmit"),
+        ("TCPDSACKIgnoredOld", ("invalid_sack", "type=retransmit")),
         # We got a duplicate SACK and discarded it.
-        "TCPDSACKIgnoredNoUndo": ("invalid_sack", "type=olddup"),
+        ("TCPDSACKIgnoredNoUndo", ("invalid_sack", "type=olddup")),
         # We received something but had to drop it because the socket's
         # receive queue was full.
-        "TCPBacklogDrop": ("receive.queue.full", None),
+        ("TCPBacklogDrop", ("receive.queue.full", None)),
         # The number of TCP segments sent containing the RST flag
-        "OutRsts": ("resets", "direction=out"),
+        ("OutRsts", ("resets", "direction=out")),
         # The total number of segments received in error (for example, bad TCP checksums).
-        "InErrs": ("errors", "direction=in"),
-    }
+        ("InErrs", ("errors", "direction=in")),
+    ])
 
     def print_netstat(statstype, metric, value, tags="", output_file_success=None):
         output_file_success = output_file_success or sys.stdout
@@ -265,7 +267,7 @@ def parse_and_print_metrics(f_netstat, f_sockstat, output_file_success=None,
         print("net.stat.%s.%s %d %s%s%s" % (statstype, metric, ts, value, space, tags),
               file=output_file_success)
 
-    statsdikt = {}
+    statsdikt = OrderedDict()
 
     ts = int(time.time())
 
