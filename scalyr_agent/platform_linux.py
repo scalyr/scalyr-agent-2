b98a68aac853774a110cb9166235442dabc690e6
arthur <arthurk@sentinelone.com>
2021-11-29 21:54:09 +0300

    Remove duplicated method. The same version of the method exists in the parent class.

--- scalyr_agent/platform_linux.py
+++ scalyr_agent/platform_linux.py
@@ -20,7 +20,6 @@ from __future__ import absolute_import
 
 __author__ = "czerwin@scalyr.com"
 
-import os
 from sys import platform as _platform
 
 if False:
@@ -28,18 +27,11 @@ if False:
 
 from scalyr_agent.json_lib import JsonObject
 from scalyr_agent.platform_posix import PosixPlatformController
-from scalyr_agent.platform_controller import DefaultPaths
 from scalyr_agent.configuration import Configuration
 from scalyr_agent.copying_manager.copying_manager import (
     WORKER_SESSION_PROCESS_MONITOR_ID_PREFIX,
 )
-
-from scalyr_agent.__scalyr__ import (
-    get_install_root,
-    TARBALL_INSTALL,
-    DEV_INSTALL,
-    PACKAGE_INSTALL,
-)
+from scalyr_agent import __scalyr__
 
 
 class LinuxPlatformController(PosixPlatformController):
@@ -60,38 +52,7 @@ class LinuxPlatformController(PosixPlatformController):
         @return:  True if this platform instance can handle the current server.
         @rtype: bool
         """
-        return _platform.lower().startswith("linux")
-
-    @property
-    def default_paths(self):
-        """Returns the default paths to use for various configuration options for this platform.
-
-        @return: The default paths
-        @rtype: DefaultPaths
-        """
-        if self._install_type == PACKAGE_INSTALL:
-            return DefaultPaths(
-                "/var/log/scalyr-agent-2",
-                "/etc/scalyr-agent-2/agent.json",
-                "/var/lib/scalyr-agent-2",
-            )
-        elif self._install_type == TARBALL_INSTALL:
-            install_location = get_install_root()
-            return DefaultPaths(
-                os.path.join(install_location, "log"),
-                os.path.join(install_location, "config", "agent.json"),
-                os.path.join(install_location, "data"),
-            )
-        else:
-            assert self._install_type == DEV_INSTALL
-            # For developers only.  We default to a directory ~/scalyr-agent-dev for storing
-            # all log/data information, and then require a log, config, and data subdirectory in each of those.
-            base_dir = os.path.join(os.path.expanduser("~"), "scalyr-agent-dev")
-            return DefaultPaths(
-                os.path.join(base_dir, "log"),
-                os.path.join(base_dir, "config", "agent.json"),
-                os.path.join(base_dir, "data"),
-            )
+        return __scalyr__.PLATFORM_TYPE == __scalyr__.PlatformType.LINUX
 
     def get_default_monitors(self, config):  # type: (Configuration) -> List
         """Returns the default monitors to use for this platform.
