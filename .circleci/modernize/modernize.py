09d5d5bdd93eb4971c45014067011b8b875d4691
arthur <arthurk@sentinelone.com>
2021-12-03 21:31:38 +0300

    reformat

--- .circleci/modernize/modernize.py
+++ .circleci/modernize/modernize.py
@@ -430,17 +430,10 @@ if __name__ == "__main__":
         os.path.join(
             root, ".circleci/docker_unified_smoke_unit/smoketest/smoketest.py"
         ),
-        *glob.glob(
-            os.path.join(root, "agent_build/**/*.py"), recursive=True
-        ),
-        *glob.glob(
-            os.path.join(root, "tests/package_tests/**/*.py"), recursive=True
-        ),
-        *glob.glob(
-            os.path.join(root, "scripts/cicd/**/*.py"), recursive=True
-        ),
-        os.path.join(root, "build_package_new.py")
-
+        *glob.glob(os.path.join(root, "agent_build/**/*.py"), recursive=True),
+        *glob.glob(os.path.join(root, "tests/package_tests/**/*.py"), recursive=True),
+        *glob.glob(os.path.join(root, "scripts/cicd/**/*.py"), recursive=True),
+        os.path.join(root, "build_package_new.py"),
     }
 
     if not args.files:
