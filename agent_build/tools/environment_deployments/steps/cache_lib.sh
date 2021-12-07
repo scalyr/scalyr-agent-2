149bcd8054e5c02956b8c569a1867de3b9fe6756
arthur <arthurk@sentinelone.com>
2021-12-08 00:50:20 +0300

    Refactor shell script caching

--- agent_build/tools/environment_deployments/steps/cache_lib.sh
+++ agent_build/tools/environment_deployments/steps/cache_lib.sh
@@ -13,18 +13,61 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# This is a primitive caching library that can be used to save or reuse some intermediate results by some
-# "Deployment Steps". See more in class "ShellScriptDeploymentStep" in the "agent_build/tools/environment_deployments/deployments.py"
+# This is a " runner" script that serves as an entry point for the scripts that are specified in the Deployment Steps
+# with shell scripts.
+# See more in class "ShellScriptDeploymentStep" in the "agent_build/tools/environment_deployments/deployments.py"
+#
+# The runner script accepts the path to the script to run as a first argument. The target script is called by using
+# 'source' command.
+# The runner script also accepts the optional cache directory as a second argument. If the cache directory is specified,
+# then it enables primitive caching mechanism that can be used by target scripts to cache their results in that
+# directory.
+
+STEP_SCRIPT_PATH="$1"
+CACHE_DIR="$2"
+
 set -e
 
-CACHE_DIR="$1"
+PARENT_DIR="$(dirname "$0")"
+SOURCE_ROOT=$(dirname "$(dirname "$(dirname "$(dirname "$PARENT_DIR")")")")
 
+function log() {
 
+  other_args="$@"
+  new_message_prefix=""
+  new_line_option=""
+  if [[ "$1" == "-nb" ]]; then
+    other_args="${@:2}"
+  else
+    new_message_prefix="# "
+  fi
+
+  if [[ "$1" == "-ne" ]];then
+    new_line_option="-n"
+    other_args="${@:2}"
+  fi
+
+  >&2 echo "$new_line_option"  "$new_message_prefix" "${other_args[@]}"
+}
+
+function sh_c() {
+
+  echo "sh -c '${*}'"
+  sh -c "${*}" > /dev/null
+}
+
+function sh_cs() {
+  sh -c "${*}"
+}
+
+log "=========== Run Deployment Step Script'$(basename "$STEP_SCRIPT_PATH")' ==========="
+
+# Check if the cache directory is specified. If it doesn't, then the caching has to be disabled.
 if [ -n "$CACHE_DIR" ]; then
   if [ ! -d "$CACHE_DIR" ]; then
     mkdir -p "${CACHE_DIR}"
   fi
-  echo "Use dir ${CACHE_DIR} as cache."
+  log "Use directory ${CACHE_DIR} as cache."
   use_cache=true
 else
   use_cache=false
@@ -35,65 +78,63 @@ fi
 # Function that restores data from cache if exists.
 function restore_from_cache() {
   if ! $use_cache ; then
-    echo "Cache disabled."
+    log "Cache disabled."
     return 0
   fi
-  name=$1
+  cache_key=$1
   path=$2
 
-  full_path="${CACHE_DIR}/${name}"
+  full_cache_path="${CACHE_DIR}/${cache_key}"
 
+  log -ne "Restore path '${path}' from cache key '${cache_key}'...  "
 
-  if [ -d "${full_path}" ]; then
-    echo "Directory ${name} in cache. Reuse it."
+  if [ -d "${full_cache_path}" ]; then
+    log -nb "found (directory)"
     mkdir -p "$(dirname "$path")"
-    cp -a "${full_path}/." "${path}"
-
-  else
-    echo "Directory ${name} not in cache"
-  fi
-  if [ -f "${full_path}" ]; then
-    echo "File ${name} in cache. Reuse it."
+    cp -a "${full_cache_path}/." "${path}"
+  elif [ -f "${full_cache_path}" ]; then
+    log -nb "found (file)"
     mkdir -p "$(dirname "$path")"
-    cp -a "${full_path}" "${path}"
-
+    cp -a "${full_cache_path}" "${path}"
   else
-    echo "File ${name} not in cache"
+    log -nb "not found."
   fi
 }
 
 
 # Function that saves data to cache if needed.
 function save_to_cache() {
-  name=$1
+  cache_key=$1
   path=$2
 
   if ! $use_cache ; then
-    echo "Cache disabled"
+    log "Cache disabled"
     return 0
   fi
 
-  full_path="${CACHE_DIR}/${name}"
+  full_cache_path="${CACHE_DIR}/${cache_key}"
 
   if [ -f "${path}" ]; then
-    if [ ! -f "${full_path}" ]; then
-      echo "File ${path} saved to cache"
-      mkdir -p "$(dirname "$full_path")"
-      cp -a "${path}" "${full_path}"
+    if [ ! -f "${full_cache_path}" ]; then
+      mkdir -p "$(dirname "$full_cache_path")"
+      log "File ${path} saved to cache directory: '${full_cache_path}'"
+      cp -a "${path}" "${full_cache_path}"
     else
-      echo "File ${path} not saved to cache"
+      log "File ${path} is already in the cache, and there's no need to save it."
     fi
   else
-    if [ ! -d "${full_path}" ]; then
-      echo "Directory ${path} saved to cache"
-      mkdir -p "$(dirname "$full_path")"
-      cp -a "${path}/." "${full_path}"
-
+    if [ ! -d "${full_cache_path}" ]; then
+      mkdir -p "$(dirname "$full_cache_path")"
+      cp -a "${path}/." "${full_cache_path}"
+      log "Directory ${path} saved to cache directory: '${full_cache_path}'"
     else
-      echo "Directory ${path} not saved to cache"
+      log "Directory ${path} is already in the cache, and there's no need to save it."
     fi
   fi
-
-
-
 }
+
+
+export SOURCE_ROOT
+source "${STEP_SCRIPT_PATH}"
+
+log "=========== The Deployment Step Script '$(basename "$STEP_SCRIPT_PATH")' is successfully ended ==========="
