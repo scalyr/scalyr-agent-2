30e983500003d4c377039d1615e47b97795f038b
arthur <arthurk@sentinelone.com>
2021-12-09 15:01:23 +0300

    Move 'use_requests_lib' option upper in the useful config options because some of the tests rely on the order.

--- scalyr_agent/configuration.py
+++ scalyr_agent/configuration.py
@@ -675,13 +675,13 @@ class Configuration(object):
             "max_log_offset_size",
             "max_existing_log_offset_size",
             "json_library",
+            "use_requests_lib",
             "use_multiprocess_workers",
             "default_sessions_per_worker",
             "default_worker_session_status_message_interval",
             "enable_worker_session_process_metrics_gather",
             # NOTE: It's important we use sanitzed_ version of this method which masks the API key
             "sanitized_worker_configs",
-            "use_requests_lib",
         ]
 
         # get options (if any) from the other configuration object
