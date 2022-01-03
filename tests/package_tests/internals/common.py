eda22f3c70046e8182c4465cc4bd202134d9322d
Tomaz Muraus <tomaz@tomaz.me>
2022-01-03 10:44:43 +0100

    Improve formatting.

--- tests/package_tests/internals/common.py
+++ tests/package_tests/internals/common.py
@@ -252,19 +252,19 @@ class AgentLogRequestStatsLineCheck(LogVerifierCheck):
                 if bytes_sent <= 0:
                     return (
                         LogVerifierCheckResult.FAIL,
-                        f"Agent log says that during the run the agent has sent zero bytes."
+                        f"Agent log says that during the run the agent has sent zero bytes.\n"
                         f"Whole log content: {whole_log_text}",
                     )
                 if requests_sent <= 0:
                     return (
                         LogVerifierCheckResult.FAIL,
-                        f"Agent log says that during the run the agent has sent zero requests."
+                        f"Agent log says that during the run the agent has sent zero requests.\n"
                         f"Whole log content: {whole_log_text}",
                     )
 
                 return (
                     LogVerifierCheckResult.SUCCESS,
-                    f"Agent requests stats have been found and they are valid."
+                    f"Agent requests stats have been found and they are valid.\n"
                     f"Whole log content: {whole_log_text}",
                 )
 
@@ -283,7 +283,7 @@ class AssertAgentLogLineIsNotAnErrorCheck(LogVerifierCheck):
             if re.match(rf"{AGENT_LOG_LINE_TIMESTAMP} ERROR .*", line):
                 return (
                     LogVerifierCheckResult.FAIL,
-                    f"Agent log contains error line : {line}. Whole log content: {whole_log_text}",
+                    f"Agent log contains error line: {line}.\nWhole log content: {whole_log_text}",
                 )
 
         return LogVerifierCheckResult.SUCCESS
