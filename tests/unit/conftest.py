ddd43f1597b0a64b777b2af64aacd45045dd1bc7
Tomaz Muraus <tomaz@tomaz.me>
2022-01-14 13:22:41 +0100

    Skip Python 3 only tests under Python 2.

---
+++ tests/unit/conftest.py
@@ -0,0 +1,23 @@
+# Copyright 2014-2022 Scalyr Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+
+import sys
+
+# We ignore Python 3 only tests and modules under Python 2
+collect_ignore = ["setup.py"]
+if sys.version_info[0] < 3:
+    collect_ignore.append("tests/unit/builtin_monitors/openmetrics_monitor_test.py")
+    collect_ignore.append("builtin_monitors/openmetrics_monitor_test.py")
