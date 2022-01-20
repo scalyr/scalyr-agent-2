1d9aa233b17bb52ecde53c0db5c27a7aa77eb2a0
Tomaz Muraus <tomaz@tomaz.me>
2022-01-20 20:35:04 +0100

    Fix copy manager functionality so it works under Python >= 3.8 on OS X (#818)
    * Fix copy manager functionality so it works under Python >= 3.8 on OS X.
    
    In Python 3.8, local objects can't be pickled anymore which means we
    need to move that class definition to a top module level scope so it can
    be pickled by the multiprocess functionality.
    
    See https://gist.github.com/Kami/8386c79d2db7c95329e6c182ec639f49 and
    https://github.com/spack/spack/issues/14102 for more details.
    
    * Use Python 3.8 for osx unit tests on CI.
    
    * Don't run multiprocess copying manager tests on OSX since it doesn't
    work there under Python 3.8.
    
    * Add unit and smoke tests for python 3.8
    
    Co-authored-by: arthur <arthurk@sentinelone.com>

--- tests/unit/copying_manager_tests/copying_manager_unittest_test.py
+++ tests/unit/copying_manager_tests/copying_manager_unittest_test.py
@@ -60,8 +60,11 @@ def pytest_addoption(parser):
 def pytest_generate_tests(metafunc):
     if "worker_type" in metafunc.fixturenames:
         test_params = ["thread"]
-        # if the OS is not Windows and python version > 2.7 then also do the multiprocess workers testing.
-        if platform.system() != "Windows" and sys.version_info >= (2, 7):
+        # if the OS is not Windows / OSX and python version > 2.7 then also do the multiprocess workers testing.
+        if platform.system() not in ["Windows", "Darwin"] and sys.version_info >= (
+            2,
+            7,
+        ):
             test_params.append("process")
 
         metafunc.parametrize("worker_type", test_params)
