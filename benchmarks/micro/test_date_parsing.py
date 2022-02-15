fef034b620a481abb2de03f977bd0ed8fffb6b63
Tomaz Muraus <tomazm@sentinelone.com>
2022-02-15 16:06:59 +0100

    Add microbenchmarks for it.

--- benchmarks/micro/test_date_parsing.py
+++ benchmarks/micro/test_date_parsing.py
@@ -38,19 +38,26 @@ from scalyr_agent.date_parsing_utils import _rfc3339_to_datetime_udatetime
 
 from .time_utils import process_time
 
-DATE_WITH_FRACTION_STR = u"2015-08-03T09:12:43.143757463Z"
-EXPECTED_RESULT_WITH_FRACTION_TIMESTAMP = 1438593163143757463
-EXPECTED_RESULT_WITH_FRACTION_DT = datetime.datetime(2015, 8, 3, 9, 12, 43, 143757)
+DATE_WITH_FRACTION_STR = u"2015-08-03T09:12:43.123456"
+EXPECTED_RESULT_WITH_FRACTION_TIMESTAMP = 1438593163123456000
+EXPECTED_RESULT_WITH_FRACTION_DT = datetime.datetime(2015, 8, 3, 9, 12, 43, 123456)
+
+DATE_WITH_FRACTION_AND_TZ_STR = u"2015-08-03T09:12:43.123456-08:00"
+EXPECTED_RESULT_WITH_FRACTION_AND_TZ_TIMESTAMP = 1438593163143757463
+EXPECTED_RESULT_WITH_FRACTION_AND_TZ_DT = datetime.datetime(2015, 8, 3, 17, 12, 43, 123456)
 
 DATE_WITHOUT_FRACTION_STR = u"2015-08-03T09:12:43"
 EXPECTED_RESULT_WITHOUT_FRACTION_TIMESTAMP = 1438593163000000000
 EXPECTED_RESULT_WITHOUT_FRACTION_DT = datetime.datetime(2015, 8, 3, 9, 12, 43)
 
+DATE_WITHOUT_FRACTION_WITH_TZ_STR = u"2015-08-03T09:12:43-08:00"
+EXPECTED_RESULT_WITHOUT_FRACTION_WITH_TZ_TIMESTAMP = 1438593163000000000
+EXPECTED_RESULT_WITHOUT_FRACTION_WITH_TZ_DT = datetime.datetime(2015, 8, 3, 17, 12, 43)
+
 # If time.process_time is available, we use that so we get more accurate and less noisy results
 # on Circle CI
 
 timer = process_time
-
 
 @pytest.mark.parametrize(
     "with_fraction", [True, False], ids=["fraction", "no_fraction"]
@@ -176,14 +183,26 @@ def test_rfc3339_to_datetime_regex(benchmark, with_fraction):
 @pytest.mark.parametrize(
     "with_fraction", [True, False], ids=["fraction", "no_fraction"]
 )
+@pytest.mark.parametrize(
+    "with_tz", [True, False], ids=["tz", "no_tz"]
+)
 @pytest.mark.benchmark(group="rfc3339_to_datetime", timer=timer)
-def test_rfc3339_to_datetime_string_split(benchmark, with_fraction):
+def test_rfc3339_to_datetime_string_split(benchmark, with_fraction, with_tz):
     if with_fraction:
-        date_str = DATE_WITH_FRACTION_STR
-        expected_result = EXPECTED_RESULT_WITH_FRACTION_DT
+        if with_tz:
+            date_str = DATE_WITH_FRACTION_AND_TZ_STR
+            expected_result = EXPECTED_RESULT_WITH_FRACTION_AND_TZ_DT
+        else:
+            date_str = DATE_WITH_FRACTION_STR
+            expected_result = EXPECTED_RESULT_WITH_FRACTION_DT
     else:
-        date_str = DATE_WITHOUT_FRACTION_STR
-        expected_result = EXPECTED_RESULT_WITHOUT_FRACTION_DT
+        if with_tz:
+            date_str = DATE_WITHOUT_FRACTION_WITH_TZ_STR
+            expected_result = EXPECTED_RESULT_WITHOUT_FRACTION_WITH_TZ_DT
+            pass
+        else:
+            date_str = DATE_WITHOUT_FRACTION_STR
+            expected_result = EXPECTED_RESULT_WITHOUT_FRACTION_DT
 
     def run_benchmark():
         result = _rfc3339_to_datetime_string_split(date_str)
@@ -196,14 +215,26 @@ def test_rfc3339_to_datetime_string_split(benchmark, with_fraction):
 @pytest.mark.parametrize(
     "with_fraction", [True, False], ids=["fraction", "no_fraction"]
 )
+@pytest.mark.parametrize(
+    "with_tz", [True, False], ids=["tz", "no_tz"]
+)
 @pytest.mark.benchmark(group="rfc3339_to_datetime", timer=timer)
-def test_rfc3339_to_datetime_udatetime(benchmark, with_fraction):
+def test_rfc3339_to_datetime_udatetime(benchmark, with_fraction, with_tz):
     if with_fraction:
-        date_str = DATE_WITH_FRACTION_STR
-        expected_result = EXPECTED_RESULT_WITH_FRACTION_DT
+        if with_tz:
+            date_str = DATE_WITH_FRACTION_AND_TZ_STR
+            expected_result = EXPECTED_RESULT_WITH_FRACTION_AND_TZ_DT
+        else:
+            date_str = DATE_WITH_FRACTION_STR
+            expected_result = EXPECTED_RESULT_WITH_FRACTION_DT
     else:
-        date_str = DATE_WITHOUT_FRACTION_STR
-        expected_result = EXPECTED_RESULT_WITHOUT_FRACTION_DT
+        if with_tz:
+            date_str = DATE_WITHOUT_FRACTION_WITH_TZ_STR
+            expected_result = EXPECTED_RESULT_WITHOUT_FRACTION_WITH_TZ_DT
+            pass
+        else:
+            date_str = DATE_WITHOUT_FRACTION_STR
+            expected_result = EXPECTED_RESULT_WITHOUT_FRACTION_DT
 
     def run_benchmark():
         result = _rfc3339_to_datetime_udatetime(date_str)
