1248f1f7320ffdd111b38e8c305ba5ad1040bece
Tomaz Muraus <tomazm@sentinelone.com>
2022-02-23 13:54:47 +0100

    Fix build packge so it works correctly under python 2.

--- build_package.py
+++ build_package.py
@@ -1345,10 +1345,9 @@ def write_to_file(string_value, file_path):
     @param string_value: The value to write to the file.
     @param file_path: The path of the file to write to.
     """
-    dest_fp = open(file_path, "w")
-    dest_fp.write(string_value.rstrip())
-    dest_fp.write(six.ensure_text(os.linesep))
-    dest_fp.close()
+    with open(file_path, "wb") as dest_fp:
+        dest_fp.write(six.ensure_binary(string_value.rstrip()))
+        dest_fp.write(six.ensure_binary(os.linesep))
 
 
 def parse_date(date_str):
