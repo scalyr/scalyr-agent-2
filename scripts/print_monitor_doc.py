383458dd537a80e3640d6fc966a2e82eb3ffaf79
Tomaz Muraus <tomazm@sentinelone.com>
2022-04-07 17:17:03 +0200

    Fix lint.

--- scripts/print_monitor_doc.py
+++ scripts/print_monitor_doc.py
@@ -162,13 +162,10 @@ def print_options(option_list, column_size):
     sys.stdout.flush()
     for row in rows:
         sys.stdout.write(
-            "| %s%s |"
-            % (row[0], space_filler(first_column_length - len(row[0]) - 1))
+            "| %s%s |" % (row[0], space_filler(first_column_length - len(row[0]) - 1))
         )
         row[1] = row[1].replace("\\n", "\\ n").replace("\\000", "\\ 000")
-        sys.stdout.write(
-            " %s | \n" % (row[1])
-        )
+        sys.stdout.write(" %s | \n" % (row[1]))
     sys.stdout.flush()
 
 
