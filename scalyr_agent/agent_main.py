scalyr/scalyr-agent-2

commit 2d097fcfe1c51e7e3e90c2cfe2fd71791a30b5fb

Refactor package type logic (#829)
* Remove MSI_INSTALL type for Windows and make it PACKAGE_INSTALL too.

* Refactor build info.

* Change repo sources for centos 8

* Refactor build_info

* Refactor build_info

* Change repo source for centos 8 ami image.

* Fix windows package.

* Comment fix

* small fixes

* use exec in the scalyr-agent-2-config bash script.

* lint

Co-authored-by: arthur <arthurk@sentinelone.com>

scalyr_agent/agent_main.py
@@ -37,6 +37,7 @@ from __future__ import absolute_import
 
 __author__ = "czerwin@scalyr.com"
 
+import platform
 import traceback
 import errno
 import gc
@@ -44,6 +45,7 @@ import os
 import sys
 import time
 import re
+import argparse
 import ssl
 from io import open
 
@@ -64,13 +66,11 @@ try:
     from __scalyr__ import scalyr_init
     from __scalyr__ import INSTALL_TYPE
     from __scalyr__ import DEV_INSTALL
-    from __scalyr__ import MSI_INSTALL
 except ImportError:
     from scalyr_agent.__scalyr__ import SCALYR_VERSION
     from scalyr_agent.__scalyr__ import scalyr_init
     from scalyr_agent.__scalyr__ import INSTALL_TYPE
     from scalyr_agent.__scalyr__ import DEV_INSTALL
-    from scalyr_agent.__scalyr__ import MSI_INSTALL
 
 # We must invoke this since we are an executable script.
 scalyr_init()
@@ -120,6 +120,7 @@ from scalyr_agent.platform_controller import (
 )
 from scalyr_agent.platform_controller import AgentNotRunning
 from scalyr_agent.build_info import get_build_revision
+from scalyr_agent import config_main
 from scalyr_agent import compat
 import scalyr_agent.monitors_manager
 
@@ -2115,24 +2116,55 @@ class WorkerThread(object):
 
 if __name__ == "__main__":
     my_controller = PlatformController.new_platform()
-    parser = OptionParser(
-        usage="Usage: scalyr-agent-2 [options] (start|stop|status|restart|condrestart|version)",
-        version="scalyr-agent v" + SCALYR_VERSION,
+
+    commands = [
+        "start",
+        "stop",
+        "status",
+        "restart",
+        "condrestart",
+        "version",
+        "config",
+    ]
+
+    if platform.system() == "Windows":
+        # If this is Windows, then also add service option.
+        # Using this option we can use windows executable as a base for the Windows Agent service.
+        # It has to save us from building multiple frozen binaries, when packaging.
+        commands.append("service")
+
+    # Create a special parser that does not print anything on error.
+    # This is needed to parse first positional argument and to decide which parser to use next.
+    # for example scalyr-agent-config parser or windows service.
+    # Also that is needed because we have to keep old behaviour of the script
+    # and when theres no any arguments, to show usage for the agent_main script.
+    class ErrorSilentParser(argparse.ArgumentParser):
+        def error(self, message):
+            pass
+
+    # Create parser that only has to parse first positional argument - our main command.
+    command_parser = ErrorSilentParser(
+        add_help=False,
     )
-    parser.add_option(
+    command_parser.add_argument("command", choices=commands)
+
+    # Also create parser for the agent command line.
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
         "-c",
         "--config-file",
         dest="config_filename",
         help="Read configuration from FILE",
         metavar="FILE",
     )
-    parser.add_option(
+    parser.add_argument(
         "--extra-config-dir",
         default=None,
         help="An extra directory to check for configuration files",
         metavar="PATH",
     )
-    parser.add_option(
+    parser.add_argument(
         "-q",
         "--quiet",
         action="store_true",
@@ -2140,7 +2172,7 @@ if __name__ == "__main__":
         default=False,
         help="Only print error messages when running the start, stop, and condrestart commands",
     )
-    parser.add_option(
+    parser.add_argument(
         "-v",
         "--verbose",
         action="store_true",
@@ -2148,7 +2180,7 @@ if __name__ == "__main__":
         default=False,
         help="For status command, prints detailed information about running agent.",
     )
-    parser.add_option(
+    parser.add_argument(
         "-H",
         "--health_check",
         action="store_true",
@@ -2156,23 +2188,21 @@ if __name__ == "__main__":
         default=False,
         help="For status command, prints health check status. Return code will be 0 for a passing check, and 2 for failing",
     )
-    parser.add_option(
+    parser.add_argument(
         "--format",
         dest="status_format",
         default="text",
         help="Format to use (text / json) for the agent status command.",
     )
 
-    parser.add_option(
-        "",
+    parser.add_argument(
         "--no-fork",
         action="store_true",
         dest="no_fork",
         default=False,
         help="For the run command, does not fork the program to the background.",
     )
-    parser.add_option(
-        "",
+    parser.add_argument(
         "--no-check-remote-server",
         action="store_true",
         dest="no_check_remote",
@@ -2183,34 +2213,33 @@ if __name__ == "__main__":
     )
     my_controller.add_options(parser)
 
-    (options, args) = parser.parse_args()
-    my_controller.consume_options(options)
+    # Parse the main command by command parser.
+    command_args, other_argv = command_parser.parse_known_args()
 
-    if len(args) < 1:
-        print(
-            'You must specify a command, such as "start", "stop", or "status".',
-            file=sys.stderr,
-        )
-        parser.print_help(sys.stderr)
-        sys.exit(1)
-    elif len(args) > 1:
-        print(
-            'Too many commands specified.  Only specify one of "start", "stop", "status".',
-            file=sys.stderr,
-        )
-        parser.print_help(sys.stderr)
-        sys.exit(1)
-    elif args[0] not in (
-        "start",
-        "stop",
-        "status",
-        "restart",
-        "condrestart",
-        "version",
-    ):
-        print('Unknown command given: "%s"' % args[0], file=sys.stderr)
-        parser.print_help(sys.stderr)
-        sys.exit(1)
+    # If command is not specified, then we just print usage of the this script.
+    if command_args.command is None:
+        parser.print_help(file=sys.stderr)
+        exit(1)
+
+    if command_args.command == "service":
+        # Windows specific command that tell to start Agent's Windows service.
+        from scalyr_agent import platform_windows
+
+        # Create fully valid command line args so the Windows service could handle it properly.
+        argv = [sys.argv[0]]
+        argv.extend(other_argv)
+        platform_windows.parse_options(argv)
+        exit(0)
+
+    if command_args.command == "config":
+        # Add the config option. So we can configure the agent from the same executable.
+        # It has to save us from building multiple frozen binaries, when packaging.
+        config_main.parse_config_options(other_argv)
+        exit(0)
+
+    options = parser.parse_args(args=other_argv)
+
+    my_controller.consume_options(options)
 
     if options.config_filename is not None and not os.path.isabs(
         options.config_filename
@@ -2220,7 +2249,7 @@ if __name__ == "__main__":
     main_rc = 1
     try:
         main_rc = ScalyrAgent(my_controller).main(
-            options.config_filename, args[0], options
+            options.config_filename, command_args.command, options
         )
     except Exception as mainExcept:
         print(six.text_type(mainExcept), file=sys.stderr)

.circleci/config.yml
@@ -2110,7 +2110,7 @@ jobs:
 
       - restore_cache:
           name: Restore Python Dependencies Cache
-          key: windows_python_installer-python-3.8.7-{{ .Branch }}-py-deps-cache
+          key: windows_python_installer-python-3.8.10-{{ .Branch }}-py-deps-cache
 
       - when:
           condition: << pipeline.parameters.agent_version >>
@@ -2164,7 +2164,7 @@ jobs:
 
       - save_cache:
           name: Save Python Dependencies Cache
-          key: windows_python_installer-python-3.8.7-{{ .Branch }}-py-deps-cache
+          key: windows_python_installer-python-3.8.10-{{ .Branch }}-py-deps-cache
           paths:
             - C:\Users\circleci\AppData\Local\pip\Cache
 

agent_build/linux/scalyr-agent-2-config
@@ -1,0 +1,5 @@
+#!/usr/bin/env sh
+
+set -e
+
+exec scalyr-agent-2 config "$@"

agent_build/package_builders.py
@@ -28,7 +28,6 @@ import sys
 import stat
 import os
 import re
-import io
 import platform
 import subprocess
 import logging
@@ -279,57 +278,69 @@ class PackageBuilder(abc.ABC):
         )
 
     @property
-    def _build_info(self) -> Optional[str]:
-        """Returns a string containing the package build info."""
+    def _build_info(self) -> Dict:
+        """Returns a dict containing the package build info."""
 
-        build_info_buffer = io.StringIO()
+        build_info = {}
 
-        # We need to execute the git command in the source root.
-        # Add in the e-mail address of the user building it.
+        original_dir = os.getcwd()
+
         try:
-            packager_email = (
-                common.check_output_with_log(
-                    "git config user.email", shell=True, cwd=str(__SOURCE_ROOT__)
+            # We need to execute the git command in the source root.
+            os.chdir(__SOURCE_ROOT__)
+            # Add in the e-mail address of the user building it.
+            try:
+                packager_email = (
+                    subprocess.check_output("git config user.email", shell=True)
+                    .decode()
+                    .strip()
+                )
+            except subprocess.CalledProcessError:
+                packager_email = "unknown"
+
+            build_info["packaged_by"] = packager_email
+
+            # Determine the last commit from the log.
+            commit_id = (
+                subprocess.check_output(
+                    "git log --summary -1 | head -n 1 | cut -d ' ' -f 2", shell=True
                 )
                 .decode()
                 .strip()
             )
-        except subprocess.CalledProcessError:
-            packager_email = "unknown"
 
-        print("Packaged by: %s" % packager_email.strip(), file=build_info_buffer)
+            build_info["latest_commit"] = commit_id
 
-        # Determine the last commit from the log.
-        commit_id = (
-            common.check_output_with_log(
-                "git log --summary -1 | head -n 1 | cut -d ' ' -f 2",
-                shell=True,
-                cwd=__SOURCE_ROOT__,
+            # Include the branch just for safety sake.
+            branch = (
+                subprocess.check_output("git branch | cut -d ' ' -f 2", shell=True)
+                .decode()
+                .strip()
             )
-            .decode()
-            .strip()
-        )
+            build_info["from_branch"] = branch
 
-        print("Latest commit: %s" % commit_id.strip(), file=build_info_buffer)
-
-        # Include the branch just for safety sake.
-        branch = (
-            common.check_output_with_log(
-                "git branch | cut -d ' ' -f 2", shell=True, cwd=__SOURCE_ROOT__
+            # Add a timestamp.
+            build_info["build_time"] = time.strftime(
+                "%Y-%m-%d %H:%M:%S UTC", time.gmtime()
             )
-            .decode()
-            .strip()
-        )
-        print("From branch: %s" % branch.strip(), file=build_info_buffer)
 
-        # Add a timestamp.
-        print(
-            "Build time: %s"
-            % str(time.strftime("%Y-%m-%d %H:%M:%S UTC", time.gmtime())),
-            file=build_info_buffer,
-        )
+            return build_info
+        finally:
+            os.chdir(original_dir)
 
-        return build_info_buffer.getvalue()
+    @property
+    def _install_info(self) -> Dict:
+        """
+        Get dict with installation info.
+        """
+        return {"build_info": self._build_info, "install_type": type(self).INSTALL_TYPE}
+
+    @property
+    def _install_info_str(self) -> str:
+        """
+        Get json serialized string with installation info.
+        """
+        return json.dumps(self._install_info, indent=4, sort_keys=True)
 
     @staticmethod
     def _add_config(
@@ -405,13 +416,12 @@ class PackageBuilder(abc.ABC):
 
         # Create package info file. It will be read by agent in order to determine the package type and install root.
         # See '__determine_install_root_and_type' function in scalyr_agent/__scalyr__.py file.
-        package_info_file = self._intermediate_results_path / "package_info.json"
+        install_info_file = self._intermediate_results_path / "install_info.json"
 
-        package_info = {"install_type": type(self).INSTALL_TYPE}
-        package_info_file.write_text(json.dumps(package_info))
+        install_info_file.write_text(self._install_info_str)
 
         # Add this package_info file in the 'scalyr_agent' package directory, near the __scalyr__.py file.
-        add_data = {str(package_info_file): "scalyr_agent"}
+        add_data = {str(install_info_file): "scalyr_agent"}
 
         # Add monitor modules as hidden imports, since they are not directly imported in the agent's code.
         all_builtin_monitor_module_names = [
@@ -559,10 +569,6 @@ class PackageBuilder(abc.ABC):
 
         self._agent_install_root_path.mkdir(parents=True)
 
-        # Write build_info file.
-        build_info_path = self._agent_install_root_path / "build_info"
-        build_info_path.write_text(self._build_info)
-
         # Copy the monitors directory.
         monitors_path = self._agent_install_root_path / "monitors"
         shutil.copytree(__SOURCE_ROOT__ / "monitors", monitors_path)
@@ -597,6 +603,10 @@ class PackageBuilder(abc.ABC):
             agent_config_executable_path.symlink_to(
                 pl.Path("..", "py", "scalyr_agent", "config_main.py")
             )
+
+            # Write install_info file inside the "scalyr_agent" package.
+            build_info_path = source_code_path / "scalyr_agent" / "install_info.json"
+            build_info_path.write_text(self._install_info_str)
 
             # Don't include the tests directories.  Also, don't include the .idea directory created by IDE.
             recursively_delete_dirs_by_name(

commit 1d1ce997e6ed23bb30e13cc7a2eacc2ca3bef60c

Remove if condition which is not needed and fix a bug.
That command line option will default to False which means that there
was a bug where we always ended up in that if branch and set
no_check_remote to True so the remote check was never performed.

Looks like a regression introduced recently in 2d097fcfe1c51e7e3e90c2cfe2fd71791a30b5fb.

scalyr_agent/agent_main.py
@@ -370,8 +370,7 @@ class ScalyrAgent(object):
             command_options.no_change_user,
         )
 
-        if command_options.no_check_remote is not None:
-            no_check_remote = True
+        no_check_remote = command_options.no_check_remote
 
         # noinspection PyBroadException
         try: