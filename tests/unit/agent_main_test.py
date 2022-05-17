scalyr/scalyr-agent-2

commit 2d097fcfe1c51e7e3e90c2cfe2fd71791a30b5fb

Refactor package type logic (#829)
* Remove MSI_INSTALL type for Windows and make it PACKAGE_INSTALL too.

* Refactor build info.

* Change repo sources for centos 8

* Refactor build_info

* Refactor build_info

* Change repo source for centos 8 ami image.

* Fix windows package.

* Comment fix

* small fixes

* use exec in the scalyr-agent-2-config bash script.

* lint

Co-authored-by: arthur <arthurk@sentinelone.com>

.circleci/config.yml
@@ -2110,7 +2110,7 @@ jobs:
 
       - restore_cache:
           name: Restore Python Dependencies Cache
-          key: windows_python_installer-python-3.8.7-{{ .Branch }}-py-deps-cache
+          key: windows_python_installer-python-3.8.10-{{ .Branch }}-py-deps-cache
 
       - when:
           condition: << pipeline.parameters.agent_version >>
@@ -2164,7 +2164,7 @@ jobs:
 
       - save_cache:
           name: Save Python Dependencies Cache
-          key: windows_python_installer-python-3.8.7-{{ .Branch }}-py-deps-cache
+          key: windows_python_installer-python-3.8.10-{{ .Branch }}-py-deps-cache
           paths:
             - C:\Users\circleci\AppData\Local\pip\Cache
 

scalyr_agent/agent_main.py
@@ -37,6 +37,7 @@ from __future__ import absolute_import
 
 __author__ = "czerwin@scalyr.com"
 
+import platform
 import traceback
 import errno
 import gc
@@ -44,6 +45,7 @@ import os
 import sys
 import time
 import re
+import argparse
 import ssl
 from io import open
 
@@ -64,13 +66,11 @@ try:
     from __scalyr__ import scalyr_init
     from __scalyr__ import INSTALL_TYPE
     from __scalyr__ import DEV_INSTALL
-    from __scalyr__ import MSI_INSTALL
 except ImportError:
     from scalyr_agent.__scalyr__ import SCALYR_VERSION
     from scalyr_agent.__scalyr__ import scalyr_init
     from scalyr_agent.__scalyr__ import INSTALL_TYPE
     from scalyr_agent.__scalyr__ import DEV_INSTALL
-    from scalyr_agent.__scalyr__ import MSI_INSTALL
 
 # We must invoke this since we are an executable script.
 scalyr_init()
@@ -120,6 +120,7 @@ from scalyr_agent.platform_controller import (
 )
 from scalyr_agent.platform_controller import AgentNotRunning
 from scalyr_agent.build_info import get_build_revision
+from scalyr_agent import config_main
 from scalyr_agent import compat
 import scalyr_agent.monitors_manager
 
@@ -2115,24 +2116,55 @@ class WorkerThread(object):
 
 if __name__ == "__main__":
     my_controller = PlatformController.new_platform()
-    parser = OptionParser(
-        usage="Usage: scalyr-agent-2 [options] (start|stop|status|restart|condrestart|version)",
-        version="scalyr-agent v" + SCALYR_VERSION,
+
+    commands = [
+        "start",
+        "stop",
+        "status",
+        "restart",
+        "condrestart",
+        "version",
+        "config",
+    ]
+
+    if platform.system() == "Windows":
+        # If this is Windows, then also add service option.
+        # Using this option we can use windows executable as a base for the Windows Agent service.
+        # It has to save us from building multiple frozen binaries, when packaging.
+        commands.append("service")
+
+    # Create a special parser that does not print anything on error.
+    # This is needed to parse first positional argument and to decide which parser to use next.
+    # for example scalyr-agent-config parser or windows service.
+    # Also that is needed because we have to keep old behaviour of the script
+    # and when theres no any arguments, to show usage for the agent_main script.
+    class ErrorSilentParser(argparse.ArgumentParser):
+        def error(self, message):
+            pass
+
+    # Create parser that only has to parse first positional argument - our main command.
+    command_parser = ErrorSilentParser(
+        add_help=False,
     )
-    parser.add_option(
+    command_parser.add_argument("command", choices=commands)
+
+    # Also create parser for the agent command line.
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
         "-c",
         "--config-file",
         dest="config_filename",
         help="Read configuration from FILE",
         metavar="FILE",
     )
-    parser.add_option(
+    parser.add_argument(
         "--extra-config-dir",
         default=None,
         help="An extra directory to check for configuration files",
         metavar="PATH",
     )
-    parser.add_option(
+    parser.add_argument(
         "-q",
         "--quiet",
         action="store_true",
@@ -2140,7 +2172,7 @@ if __name__ == "__main__":
         default=False,
         help="Only print error messages when running the start, stop, and condrestart commands",
     )
-    parser.add_option(
+    parser.add_argument(
         "-v",
         "--verbose",
         action="store_true",
@@ -2148,7 +2180,7 @@ if __name__ == "__main__":
         default=False,
         help="For status command, prints detailed information about running agent.",
     )
-    parser.add_option(
+    parser.add_argument(
         "-H",
         "--health_check",
         action="store_true",
@@ -2156,23 +2188,21 @@ if __name__ == "__main__":
         default=False,
         help="For status command, prints health check status. Return code will be 0 for a passing check, and 2 for failing",
     )
-    parser.add_option(
+    parser.add_argument(
         "--format",
         dest="status_format",
         default="text",
         help="Format to use (text / json) for the agent status command.",
     )
 
-    parser.add_option(
-        "",
+    parser.add_argument(
         "--no-fork",
         action="store_true",
         dest="no_fork",
         default=False,
         help="For the run command, does not fork the program to the background.",
     )
-    parser.add_option(
-        "",
+    parser.add_argument(
         "--no-check-remote-server",
         action="store_true",
         dest="no_check_remote",
@@ -2183,34 +2213,33 @@ if __name__ == "__main__":
     )
     my_controller.add_options(parser)
 
-    (options, args) = parser.parse_args()
-    my_controller.consume_options(options)
+    # Parse the main command by command parser.
+    command_args, other_argv = command_parser.parse_known_args()
 
-    if len(args) < 1:
-        print(
-            'You must specify a command, such as "start", "stop", or "status".',
-            file=sys.stderr,
-        )
-        parser.print_help(sys.stderr)
-        sys.exit(1)
-    elif len(args) > 1:
-        print(
-            'Too many commands specified.  Only specify one of "start", "stop", "status".',
-            file=sys.stderr,
-        )
-        parser.print_help(sys.stderr)
-        sys.exit(1)
-    elif args[0] not in (
-        "start",
-        "stop",
-        "status",
-        "restart",
-        "condrestart",
-        "version",
-    ):
-        print('Unknown command given: "%s"' % args[0], file=sys.stderr)
-        parser.print_help(sys.stderr)
-        sys.exit(1)
+    # If command is not specified, then we just print usage of the this script.
+    if command_args.command is None:
+        parser.print_help(file=sys.stderr)
+        exit(1)
+
+    if command_args.command == "service":
+        # Windows specific command that tell to start Agent's Windows service.
+        from scalyr_agent import platform_windows
+
+        # Create fully valid command line args so the Windows service could handle it properly.
+        argv = [sys.argv[0]]
+        argv.extend(other_argv)
+        platform_windows.parse_options(argv)
+        exit(0)
+
+    if command_args.command == "config":
+        # Add the config option. So we can configure the agent from the same executable.
+        # It has to save us from building multiple frozen binaries, when packaging.
+        config_main.parse_config_options(other_argv)
+        exit(0)
+
+    options = parser.parse_args(args=other_argv)
+
+    my_controller.consume_options(options)
 
     if options.config_filename is not None and not os.path.isabs(
         options.config_filename
@@ -2220,7 +2249,7 @@ if __name__ == "__main__":
     main_rc = 1
     try:
         main_rc = ScalyrAgent(my_controller).main(
-            options.config_filename, args[0], options
+            options.config_filename, command_args.command, options
         )
     except Exception as mainExcept:
         print(six.text_type(mainExcept), file=sys.stderr)

tests/ami/scripts/test_windows.ps1.j2
@@ -91,7 +91,7 @@ Log-Message "agent.json and agent.d/ config files permissions checks"
 Log-Message ""
 
 # NOTE: At this point fix doesn't run automatically post install so we manually invoke the scrupt and verify it works
-& "C:\Program Files (x86)\Scalyr\bin\scalyr-agent-2-config.exe" "--fix-config-permissions"
+& "C:\Program Files (x86)\Scalyr\bin\scalyr-agent-2-config" "--fix-config-permissions"
 
 dir "C:\Program Files (x86)\Scalyr\config"
 dir "C:\Program Files (x86)\Scalyr\config\agent.d"
@@ -176,19 +176,6 @@ Log-Message ""
 Log-Message "native agent status output verified"
 Log-Message ""
 
-#
-# Verify build_info file exists
-#
-
-Log-Message ""
-Log-Message "Verifying build_info file exists"
-Log-Message ""
-
-type "C:\Program Files (x86)\Scalyr\bin\build_info"
-
-Log-Message ""
-Log-Message "build_info validation completed"
-Log-Message ""
 
 #
 # Perform basic Windows Event Log sanity checks

tests/unit/agent_main_test.py
@@ -19,6 +19,7 @@ from __future__ import unicode_literals
 import os
 import sys
 import subprocess
+import platform
 import re
 from io import open
 import functools
@@ -31,6 +32,8 @@ from scalyr_agent import __scalyr__
 from scalyr_agent.test_base import BaseScalyrLogCaptureTestCase
 from scalyr_agent import agent_main, agent_status
 from scalyr_agent.scalyr_client import create_client
+
+import pytest
 
 __all__ = ["AgentMainTestCase"]
 
@@ -51,6 +54,9 @@ AGENT_MAIN = os.path.abspath(os.path.join(BASE_DIR, "../../scalyr_agent/agent_ma
 
 
 class AgentMainTestCase(BaseScalyrLogCaptureTestCase):
+    @pytest.mark.skipif(
+        platform.system() == "Windows", reason="This test is not for Windows."
+    )
     @mock.patch("scalyr_agent.__scalyr__.INSTALL_TYPE", __scalyr__.PACKAGE_INSTALL)
     def test_create_client_ca_file_and_intermediate_certs_file_doesnt_exist(self):
         from scalyr_agent.agent_main import ScalyrAgent
@@ -103,7 +109,7 @@ class AgentMainTestCase(BaseScalyrLogCaptureTestCase):
             ValueError, expected_msg, functools.partial(create_client, config)
         )
 
-    def test_ca_cert_files_checks_are_skipped_under_dev_and_msi_install(self):
+    def test_ca_cert_files_checks_are_skipped_under_dev(self):
         # Skip those checks under dev and msi install because those final generated certs files
         # are not present under dev install
 
@@ -134,8 +140,18 @@ class AgentMainTestCase(BaseScalyrLogCaptureTestCase):
 
             self.assertTrue(create_client(config=config))
 
+    @pytest.mark.skipif(
+        platform.system() != "Windows", reason="This test has to run on Windows."
+    )
+    def test_ca_cert_files_checks_are_skipped_under_windows(self):
+
+        from scalyr_agent.agent_main import ScalyrAgent
+        from scalyr_agent.platform_controller import PlatformController
+
         # 2. MSI install (only intermediate_certs_path check should be skipped)
-        with mock.patch("scalyr_agent.__scalyr__.INSTALL_TYPE", __scalyr__.MSI_INSTALL):
+        with mock.patch(
+            "scalyr_agent.__scalyr__.INSTALL_TYPE", __scalyr__.PACKAGE_INSTALL
+        ):
 
             config = mock.Mock()
             config.scalyr_server = "foo.bar.com"

agent_build/linux/scalyr-agent-2-config
@@ -1,0 +1,5 @@
+#!/usr/bin/env sh
+
+set -e
+
+exec scalyr-agent-2 config "$@"

agent_build/package_builders.py
@@ -28,7 +28,6 @@ import sys
 import stat
 import os
 import re
-import io
 import platform
 import subprocess
 import logging
@@ -279,57 +278,69 @@ class PackageBuilder(abc.ABC):
         )
 
     @property
-    def _build_info(self) -> Optional[str]:
-        """Returns a string containing the package build info."""
+    def _build_info(self) -> Dict:
+        """Returns a dict containing the package build info."""
 
-        build_info_buffer = io.StringIO()
+        build_info = {}
 
-        # We need to execute the git command in the source root.
-        # Add in the e-mail address of the user building it.
+        original_dir = os.getcwd()
+
         try:
-            packager_email = (
-                common.check_output_with_log(
-                    "git config user.email", shell=True, cwd=str(__SOURCE_ROOT__)
+            # We need to execute the git command in the source root.
+            os.chdir(__SOURCE_ROOT__)
+            # Add in the e-mail address of the user building it.
+            try:
+                packager_email = (
+                    subprocess.check_output("git config user.email", shell=True)
+                    .decode()
+                    .strip()
+                )
+            except subprocess.CalledProcessError:
+                packager_email = "unknown"
+
+            build_info["packaged_by"] = packager_email
+
+            # Determine the last commit from the log.
+            commit_id = (
+                subprocess.check_output(
+                    "git log --summary -1 | head -n 1 | cut -d ' ' -f 2", shell=True
                 )
                 .decode()
                 .strip()
             )
-        except subprocess.CalledProcessError:
-            packager_email = "unknown"
 
-        print("Packaged by: %s" % packager_email.strip(), file=build_info_buffer)
+            build_info["latest_commit"] = commit_id
 
-        # Determine the last commit from the log.
-        commit_id = (
-            common.check_output_with_log(
-                "git log --summary -1 | head -n 1 | cut -d ' ' -f 2",
-                shell=True,
-                cwd=__SOURCE_ROOT__,
+            # Include the branch just for safety sake.
+            branch = (
+                subprocess.check_output("git branch | cut -d ' ' -f 2", shell=True)
+                .decode()
+                .strip()
             )
-            .decode()
-            .strip()
-        )
+            build_info["from_branch"] = branch
 
-        print("Latest commit: %s" % commit_id.strip(), file=build_info_buffer)
-
-        # Include the branch just for safety sake.
-        branch = (
-            common.check_output_with_log(
-                "git branch | cut -d ' ' -f 2", shell=True, cwd=__SOURCE_ROOT__
+            # Add a timestamp.
+            build_info["build_time"] = time.strftime(
+                "%Y-%m-%d %H:%M:%S UTC", time.gmtime()
             )
-            .decode()
-            .strip()
-        )
-        print("From branch: %s" % branch.strip(), file=build_info_buffer)
 
-        # Add a timestamp.
-        print(
-            "Build time: %s"
-            % str(time.strftime("%Y-%m-%d %H:%M:%S UTC", time.gmtime())),
-            file=build_info_buffer,
-        )
+            return build_info
+        finally:
+            os.chdir(original_dir)
 
-        return build_info_buffer.getvalue()
+    @property
+    def _install_info(self) -> Dict:
+        """
+        Get dict with installation info.
+        """
+        return {"build_info": self._build_info, "install_type": type(self).INSTALL_TYPE}
+
+    @property
+    def _install_info_str(self) -> str:
+        """
+        Get json serialized string with installation info.
+        """
+        return json.dumps(self._install_info, indent=4, sort_keys=True)
 
     @staticmethod
     def _add_config(
@@ -405,13 +416,12 @@ class PackageBuilder(abc.ABC):
 
         # Create package info file. It will be read by agent in order to determine the package type and install root.
         # See '__determine_install_root_and_type' function in scalyr_agent/__scalyr__.py file.
-        package_info_file = self._intermediate_results_path / "package_info.json"
+        install_info_file = self._intermediate_results_path / "install_info.json"
 
-        package_info = {"install_type": type(self).INSTALL_TYPE}
-        package_info_file.write_text(json.dumps(package_info))
+        install_info_file.write_text(self._install_info_str)
 
         # Add this package_info file in the 'scalyr_agent' package directory, near the __scalyr__.py file.
-        add_data = {str(package_info_file): "scalyr_agent"}
+        add_data = {str(install_info_file): "scalyr_agent"}
 
         # Add monitor modules as hidden imports, since they are not directly imported in the agent's code.
         all_builtin_monitor_module_names = [
@@ -559,10 +569,6 @@ class PackageBuilder(abc.ABC):
 
         self._agent_install_root_path.mkdir(parents=True)
 
-        # Write build_info file.
-        build_info_path = self._agent_install_root_path / "build_info"
-        build_info_path.write_text(self._build_info)
-
         # Copy the monitors directory.
         monitors_path = self._agent_install_root_path / "monitors"
         shutil.copytree(__SOURCE_ROOT__ / "monitors", monitors_path)
@@ -597,6 +603,10 @@ class PackageBuilder(abc.ABC):
             agent_config_executable_path.symlink_to(
                 pl.Path("..", "py", "scalyr_agent", "config_main.py")
             )
+
+            # Write install_info file inside the "scalyr_agent" package.
+            build_info_path = source_code_path / "scalyr_agent" / "install_info.json"
+            build_info_path.write_text(self._install_info_str)
 
             # Don't include the tests directories.  Also, don't include the .idea directory created by IDE.
             recursively_delete_dirs_by_name(

commit 02e9fcc2049d3cbcd98e42f3c1aa618d5fe4d205

Fix no check remote agent start up regression / bug (#880)
* Remove if condition which is not needed and fix a bug.

That command line option will default to False which means that there
was a bug where we always ended up in that if branch and set
no_check_remote to True so the remote check was never performed.

Looks like a regression introduced recently in 2d097fcfe1c51e7e3e90c2cfe2fd71791a30b5fb.

* Add changelog entry.

* Move check to the correct place check_remote_if_no_tty should have
priority if no tty is available.

* Update checks to verify that check_remote_if_no_tty has higher priority
than command line arg value.

* Add longer sleep so the agent has more time to pick out the files.

* Fix unit tests and make sure we always control the value of istatty.
Tests depend on the presence of tty and this means they may either fail
or pass on CI depending if the tty is present which could differ from
the local environment.

.circleci/config.yml
@@ -2486,6 +2486,22 @@ workflows:
    jobs:
      - send-circle-ci-usage-report
 
+ hourly-windows-ami-tests:
+   triggers:
+     - schedule:
+         cron: "0 * * * *"
+         filters:
+           branches:
+             only:
+               - win_py30_changes
+   jobs:
+     - ami-tests-development-windows:
+         context: scalyr-agent
+         requires:
+            - build-windows-package
+     - build-windows-package
+     - build-linux-packages-and-installer-script
+
  # We run AMI based package install tests which utilize installer script on
  # daily basis.
  # This helps us detect various installer script and other potential upstream

scalyr_agent/agent_main.py
@@ -325,6 +325,8 @@ class ScalyrAgent(object):
 
         self.__config_file_path = config_file_path
 
+        no_check_remote = command_options.no_check_remote
+
         try:
             log_warnings = command not in ["status", "stop"]
             self.__config = self.__read_and_verify_config(
@@ -369,9 +371,6 @@ class ScalyrAgent(object):
             os.getcwd(),
             command_options.no_change_user,
         )
-
-        if command_options.no_check_remote is not None:
-            no_check_remote = True
 
         # noinspection PyBroadException
         try:

tests/ami/scripts/test_windows.ps1.j2
@@ -266,10 +266,12 @@ Log-Message "Verifying agent.log"
 Log-Message ""
 
 $log_output=type "C:\Program Files (x86)\Scalyr\log\agent.log"
+echo $log_output
 
 if ("$log_output" -notmatch "ca_cert_path: C:\\invalid\\ca_certs.crt") { Throw "Invalid ca cert path, agent shouldnt have started" };
 if ("$log_output" -notmatch "failedAgentMain") { Throw "Invalid ca cert path, agent shouldnt have started" };
 if ("$log_output" -notmatch 'Invalid path "C:\\invalid\\ca_certs.crt" specified for the "ca_cert_path"') { Throw "Invalid ca cert path, agent shouldnt have started" };
+#if ("$log_output" -notmatch 'Terminating agent, please fix the error and restart the agent') { Throw "Invalid ca cert path, agent shouldnt have started" };
 
 Log-Message ""
 Log-Message "agent.log output successfully verified"
@@ -427,7 +429,7 @@ For ($i=0; $i -le $files_to_create; $i++) {
     #Add-Content c:\logs\test$i.txt.log "line $i"
 }
 
-Start-Sleep 5
+Start-Sleep 10
 
 Log-Message ""
 Log-Message "Verifying agent status output"

tests/unit/agent_main_test.py
@@ -350,8 +350,78 @@ class AgentMainTestCase(BaseScalyrLogCaptureTestCase):
                     ".*" + re.escape("Warning, skipping copying log lines.")
                 )
 
+    def test_no_check_remote_command_line_option(self):
+        from scalyr_agent.agent_main import ScalyrAgent
+
+        mock_isatty_true = mock.Mock(return_value=True)
+
+        mock_config = mock.Mock()
+
+        platform_controller = mock.Mock()
+        platform_controller.default_paths = mock.Mock()
+
+        agent = ScalyrAgent(platform_controller)
+        agent._ScalyrAgent__config = mock_config
+        agent._ScalyrAgent__read_and_verify_config = mock.Mock()
+        agent._ScalyrAgent__perform_config_checks = mock.Mock()
+        agent._ScalyrAgent__run = mock.Mock()
+
+        agent._ScalyrAgent__read_and_verify_config.return_value = mock_config
+
+        agent._ScalyrAgent__run.return_value = 7
+        mock_config_path = self._write_mock_config()
+
+        # should use default value of False
+        mock_command = "inner_run_with_checks"
+        mock_command_options = mock.Mock()
+        mock_command_options.no_check_remote = False
+
+        mock_stdout = mock.Mock()
+        mock_stdout.isatty = mock_isatty_true
+
+        with mock.patch("scalyr_agent.agent_main.sys.stdout", mock_stdout):
+            return_code = agent.main(
+                mock_config_path, mock_command, mock_command_options
+            )
+            self.assertEqual(return_code, 7)
+            agent._ScalyrAgent__perform_config_checks.assert_called_with(False)
+
+        mock_config = mock.Mock()
+
+        platform_controller = mock.Mock()
+        platform_controller.default_paths = mock.Mock()
+
+        agent = ScalyrAgent(platform_controller)
+        agent._ScalyrAgent__config = mock_config
+        agent._ScalyrAgent__read_and_verify_config = mock.Mock()
+        agent._ScalyrAgent__perform_config_checks = mock.Mock()
+        agent._ScalyrAgent__run = mock.Mock()
+
+        agent._ScalyrAgent__read_and_verify_config.return_value = mock_config
+
+        agent._ScalyrAgent__run.return_value = 7
+        mock_config_path = self._write_mock_config()
+
+        # should use overridden value of True
+        mock_command = "inner_run_with_checks"
+        mock_command_options = mock.Mock()
+        mock_command_options.no_check_remote = True
+
+        mock_stdout = mock.Mock()
+        mock_stdout.isatty = mock_isatty_true
+
+        with mock.patch("scalyr_agent.agent_main.sys.stdout", mock_stdout):
+            return_code = agent.main(
+                mock_config_path, mock_command, mock_command_options
+            )
+            self.assertEqual(return_code, 7)
+            agent._ScalyrAgent__perform_config_checks.assert_called_with(True)
+
     def test_check_remote_if_no_tty(self):
         from scalyr_agent.agent_main import ScalyrAgent
+
+        mock_isatty_true = mock.Mock(return_value=True)
+        mock_isatty_false = mock.Mock(return_value=False)
 
         mock_config = mock.Mock()
 
@@ -374,31 +444,41 @@ class AgentMainTestCase(BaseScalyrLogCaptureTestCase):
         mock_command_options = mock.Mock()
         mock_command_options.no_check_remote = True
 
-        return_code = agent.main(mock_config_path, mock_command, mock_command_options)
-        self.assertEqual(return_code, 7)
-        agent._ScalyrAgent__perform_config_checks.assert_called_with(True)
+        mock_stdout = mock.Mock()
+        mock_stdout.isatty = mock_isatty_true
+
+        with mock.patch("scalyr_agent.agent_main.sys.stdout", mock_stdout):
+            return_code = agent.main(
+                mock_config_path, mock_command, mock_command_options
+            )
+            self.assertEqual(return_code, 7)
+            agent._ScalyrAgent__perform_config_checks.assert_called_with(True)
 
         # tty is available, command option is not set, should default to False
         agent._ScalyrAgent__perform_config_checks.reset_mock()
 
         mock_command = "inner_run_with_checks"
         mock_command_options = mock.Mock()
-        mock_command_options.no_check_remote = None
+        mock_command_options.no_check_remote = False
 
-        return_code = agent.main(mock_config_path, mock_command, mock_command_options)
-        self.assertEqual(return_code, 7)
-        agent._ScalyrAgent__perform_config_checks.assert_called_with(False)
+        mock_stdout = mock.Mock()
+        mock_stdout.isatty = mock_isatty_true
+
+        with mock.patch("scalyr_agent.agent_main.sys.stdout", mock_stdout):
+            return_code = agent.main(
+                mock_config_path, mock_command, mock_command_options
+            )
+            self.assertEqual(return_code, 7)
+            agent._ScalyrAgent__perform_config_checks.assert_called_with(False)
 
         # tty is not available (stdout.isatty returns False), should use check_remote_if_no_tty
         # config option value (config option is set to True)
         agent._ScalyrAgent__perform_config_checks.reset_mock()
 
-        mock_command_options.no_check_remote = None
+        mock_command_options.no_check_remote = False
 
         mock_stdout = mock.Mock()
-        mock_isatty = mock.Mock()
-        mock_isatty.return_value = False
-        mock_stdout.isatty = mock_isatty
+        mock_stdout.isatty = mock_isatty_false
 
         mock_config.check_remote_if_no_tty = True
 
@@ -413,12 +493,11 @@ class AgentMainTestCase(BaseScalyrLogCaptureTestCase):
         # config option value (config option is set to False)
         agent._ScalyrAgent__perform_config_checks.reset_mock()
 
-        mock_command_options.no_check_remote = None
+        mock_command_options.no_check_remote = False
 
         mock_stdout = mock.Mock()
         mock_isatty = mock.Mock()
-        mock_isatty.return_value = False
-        mock_stdout.isatty = mock_isatty
+        mock_stdout.isatty = mock_isatty_false
 
         mock_config.check_remote_if_no_tty = False
 
@@ -433,7 +512,7 @@ class AgentMainTestCase(BaseScalyrLogCaptureTestCase):
         # config option value (config option is set to False)
         agent._ScalyrAgent__perform_config_checks.reset_mock()
 
-        mock_command_options.no_check_remote = None
+        mock_command_options.no_check_remote = False
 
         mock_stdout = mock.Mock()
         mock_isatty = None