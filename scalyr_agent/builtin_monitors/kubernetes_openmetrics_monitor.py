174ff38e954981cfe6f7153c17601e610e8cbdec
Tomaz Muraus <tomazm@sentinelone.com>
2022-05-26 18:02:04 +0200

    Remove flag which doesn't work under Python 3.6 under which we run tests.

--- scalyr_agent/builtin_monitors/kubernetes_openmetrics_monitor.py
+++ scalyr_agent/builtin_monitors/kubernetes_openmetrics_monitor.py
@@ -451,10 +451,10 @@ class TemplateWithSpecialCharacters(Template):
     delimiter = "$"
     pattern = r"""
     \$(?:
-      (?P<escaped>\$)                             |   # Escape sequence of two delimiters
-      (?P<named>(?a:[_a-z][_a-z0-9\-\.\/]*))      |   # delimiter and a Python identifier
-      {(?P<braced>(?a:[_a-z][_a-z0-9\-\.\/]*))}   |   # delimiter and a braced identifier
-      (?P<invalid>)                                   # Other ill-formed delimiter exprs
+      (?P<escaped>\$)                        |   # Escape sequence of two delimiters
+      (?P<named>[_a-z][_a-z0-9\-\.\/]*)      |   # delimiter and a Python identifier
+      {(?P<braced>[_a-z][_a-z0-9\-\.\/]*)}   |   # delimiter and a braced identifier
+      (?P<invalid>)                              # Other ill-formed delimiter exprs
     )
     """
 
