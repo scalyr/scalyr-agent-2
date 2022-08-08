265b8d4b0d2982fdab8ea8bc00d08899e28866da
arthur <arthurk@sentinelone.com>
2022-08-08 17:58:26 +0300

    test

--- agent_build/__init__.py
+++ agent_build/__init__.py
@@ -1,9 +1,9 @@
-from agent_build.tools.environment_deployments.deployments import CacheableBuilder, ShellScriptDeploymentStep
+from agent_build.tools.environment_deployments.deployments import CacheableBuilder, ShellScriptDeploymentStep, EnvironmentShellScriptStep
 from agent_build.tools.constants import Architecture, SOURCE_ROOT
 
 
 # Step that runs small script which installs requirements for the test/dev environment.
-INSTALL_TEST_REQUIREMENT_STEP = ShellScriptDeploymentStep(
+INSTALL_TEST_REQUIREMENT_STEP = EnvironmentShellScriptStep(
     name="install_test_requirements",
     script_path= SOURCE_ROOT / "agent_build/tools/environment_deployments/steps/deploy-test-environment.sh",
     tracked_file_globs=[SOURCE_ROOT / "agent_build/requirement-files/*.txt"],
