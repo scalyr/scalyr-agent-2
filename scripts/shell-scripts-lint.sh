e629218612ecba873fded86d4c8aa823ce44bdb8
arthur <arthurk@sentinelone.com>
2022-08-18 11:55:10 +0300

    fix shell check

--- scripts/shell-scripts-lint.sh
+++ scripts/shell-scripts-lint.sh
@@ -42,4 +42,4 @@ shellcheck -S info benchmarks/scripts/*.sh
 shellcheck -S info installer/scripts/*.sh
 shellcheck -S info scripts/*.sh
 shellcheck -S info scripts/circleci/*.sh
-shellcheck -S info agent_build_refactored/**/*.sh
+shellcheck -S info $(find agent_build_refactored -name "*.sh")
