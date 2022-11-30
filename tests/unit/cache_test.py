bb2d486adf505ba0f6cd5e4193c12fe7b01e9322
Joseph Makar <joseph.makar@sentinelone.com>
2022-11-30 11:05:07 -0500

    DSET-1106: PR feedback changes

---
+++ tests/unit/cache_test.py
@@ -0,0 +1,70 @@
+# Copyright 2022 Scalyr Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from scalyr_agent.builtin_monitors.windows_event_log_monitor import Cache
+from scalyr_agent.test_base import BaseScalyrLogCaptureTestCase
+
+import time
+
+class CacheTest(BaseScalyrLogCaptureTestCase):
+    def test_fixed_size(self):
+        cache = Cache(3, 3600)
+
+        cache['a'] = 1
+        cache['b'] = 2
+        cache['c'] = 3
+
+        self.assertTrue(len(cache) == 3)
+        self.assertTrue('a' in cache)
+
+        cache['d'] = 4
+
+        self.assertTrue(len(cache) == 3)
+        self.assertTrue('a' not in cache)
+
+    def test_ttl_via_contains(self):
+        cache = Cache(1000, 1)
+
+        cache['a'] = 1
+        self.assertTrue('a' in cache)
+
+        time.sleep(1)
+        self.assertTrue('a' not in cache)
+
+    def test_ttl_via_iter(self):
+        cache = Cache(1000, 1)
+
+        cache['a'] = 1
+        self.assertTrue('a' in cache)
+
+        time.sleep(1)
+        self.assertTrue(len([k for k in cache]) == 0)
+
+    def test_ttl_via_values(self):
+        cache = Cache(1000, 1)
+
+        cache['a'] = 1
+        self.assertTrue('a' in cache)
+
+        time.sleep(1)
+        self.assertTrue(len([v for v in cache.values()]) == 0)
+
+    def test_ttl_via_len(self):
+        cache = Cache(1000, 1)
+
+        cache['a'] = 1
+        self.assertTrue('a' in cache)
+
+        time.sleep(1)
+        self.assertTrue(len(cache) == 0)
