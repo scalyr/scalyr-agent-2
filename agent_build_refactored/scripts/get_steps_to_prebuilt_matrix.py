270adf04e01e067c78c11e9b4f9a7e1bffb7c41b
arthur <arthurk@sentinelone.com>
2022-12-08 14:04:11 +0100

    test

--- agent_build_refactored/scripts/get_steps_to_prebuilt_matrix.py
+++ agent_build_refactored/scripts/get_steps_to_prebuilt_matrix.py
@@ -31,50 +31,54 @@ MATRICES_PATH = pl.Path(os.environ["MATRICES_PATH"])
 
 used_builders = []
 
-existing_runners = collections.defaultdict(dict)
+existing_runners = {}
+builders_to_prebuilt_runners = collections.defaultdict(list)
 for name, runner_cls in ALL_USED_BUILDERS.items():
     for step in runner_cls.get_all_cacheable_steps():
         if not step.github_actions_settings.pre_build_in_separate_job:
             continue
 
-        if step.id in existing_runners[name]:
-            continue
+        StepWrapperRunner = existing_runners.get(step.id)
+        if StepWrapperRunner is None:
+            # Create "dummy" Runner for each runner step that has to be pre-built, this dummy runner will be executed
+            # by its fqdn to run the step.
+            class StepWrapperRunner(Runner):
+                REQUIRED_STEPS = [step]
 
-        # Create "dummy" Runner for each runner step that has to be pre-built, this dummy runner will be executed
-        # by its fqdn to run the step.
-        class StepWrapperRunner(Runner):
-            REQUIRED_STEPS = [step]
+            # Since this runner class is created dynamically we have to generate a constant fqdn for it.
+            StepWrapperRunner.assign_fully_qualified_name(
+                class_name="pre-built-",
+                module_name=__name__,
+                class_name_suffix=step.id,
+            )
+            existing_runners[step.id] = StepWrapperRunner
 
-        # Since this runner class is created dynamically we have to generate a constant fqdn for it.
-        StepWrapperRunner.assign_fully_qualified_name(
-            class_name="pre-built-",
-            module_name=__name__,
-            class_name_suffix=step.id,
+        builders_to_prebuilt_runners[name].append(
+            StepWrapperRunner.get_fully_qualified_name()
         )
-
-        existing_runners[name][step.id] = StepWrapperRunner
 
 
 if __name__ == '__main__':
 
     result_matrix = {"include": []}
 
+    pre_built_runners = {}
     for matrix_file in MATRICES_PATH.glob("*.json"):
         matrix = json.loads(matrix_file.read_text())
         for job in matrix["include"]:
             builder_name = job["name"]
 
-            if builder_name not in existing_runners:
-                continue
+            for runner in builders_to_prebuilt_runners[builder_name]:
+                pre_built_runners[runner.get_fully_qualified_name()] = runner
 
-            for runner in existing_runners[builder_name]:
-                result_matrix["include"].append(
-                    {
-                        "name": f"Pre-build: {runner.REQUIRED_STEPS[0].name}",
-                        "step-runner-fqdn": runner.get_fully_qualified_name(),
-                        "os": "ubuntu-20.04",
-                        "python-version": "3.8.13",
-                    }
-                )
+    for fqdn, runner in pre_built_runners.items():
+        result_matrix["include"].append(
+            {
+                "name": f"Pre-build: {runner.REQUIRED_STEPS[0].name}",
+                "step-runner-fqdn": fqdn,
+                "os": "ubuntu-20.04",
+                "python-version": "3.8.13",
+            }
+        )
 
     print(json.dumps(result_matrix))
