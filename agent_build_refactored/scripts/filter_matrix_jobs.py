0896974089c106ac251e7f731f27c9d6463a7a98
arthur <arthurk@sentinelone.com>
2022-12-08 19:23:28 +0100

    test

--- agent_build_refactored/scripts/filter_matrix_jobs.py
+++ agent_build_refactored/scripts/filter_matrix_jobs.py
@@ -25,7 +25,7 @@ import pathlib as pl
 
 # This file can be executed as script. Add source root to the PYTHONPATH in order to be able to import
 # local packages. All such imports also have to be done after that.
-sys.path.append(str(pl.Path(__file__).parent.parent.parent.parent))
+sys.path.append(str(pl.Path(__file__).parent.parent.parent))
 
 from agent_build_refactored import ALL_USED_BUILDERS
 
