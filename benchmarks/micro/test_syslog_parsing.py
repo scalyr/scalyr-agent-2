1c13b51785a0e33d2349edef0309bb6bc57ab7f3
Tomaz Muraus <tomaz@tomaz.me>
2023-01-27 14:44:20 +0100

    This file is not compatible with Python 2.7 so don't run it when running benchmarks under Pythin 2.7.

--- benchmarks/micro/test_syslog_parsing.py
+++ benchmarks/micro/test_syslog_parsing.py
@@ -22,103 +22,107 @@ TODO: Also exercise the following scenarios / code paths:
 * Log watcher management, file creation, expiration, locking
 """
 
-import os
-import tempfile
+# Workaround since this file is not Python 2 compatible
+import sys
 
-import mock
-import pytest
-from faker import Faker
+if sys.version_info < (3, 5, 0):
+    import os
+    import tempfile
 
-from scalyr_agent.builtin_monitors.syslog_monitor import SyslogHandler
+    import mock
+    import pytest
+    from faker import Faker
 
-MOCK_MESSAGE =  "<34>Oct 11 22:14:15 mymachine su: \'su root\' failed for lonvick on /dev/pts/8"
+    from scalyr_agent.builtin_monitors.syslog_monitor import SyslogHandler
 
-# NOTE: We use faker + same static seed to ensure the result is fully repetable / deterministic
-FAKER = Faker()
-Faker.seed(1000)
+    MOCK_MESSAGE =  "<34>Oct 11 22:14:15 mymachine su: \'su root\' failed for lonvick on /dev/pts/8"
 
-class MockConfig(object):
-    def __init__(self, values):
-        self._values = values
+    # NOTE: We use faker + same static seed to ensure the result is fully repetable / deterministic
+    FAKER = Faker()
+    Faker.seed(1000)
 
-    def get(self, key):
-        try:
-            return self._values[key]
-        except KeyError:
-            return None
+    class MockConfig(object):
+        def __init__(self, values):
+            self._values = values
 
-TEMP_DIRECTORY = tempfile.gettempdir()
+        def get(self, key):
+            try:
+                return self._values[key]
+            except KeyError:
+                return None
 
-MOCK_EXTRAS = []
+    TEMP_DIRECTORY = tempfile.gettempdir()
 
-for i in range(0, 1000):
-    extra = {
-        "proto": FAKER.random_element(["tcp", "udp"]),
-        "srcip": "127.0.0." + str(FAKER.random_int(0, 250)),
-        "destport": str(FAKER.random_int(10000, 60000))
-    }
-    MOCK_EXTRAS.append(extra)
+    MOCK_EXTRAS = []
 
-
-# fmt: off
-@pytest.mark.parametrize(
-    "message_template",
-    [
-        True,
-        False,
-    ],
-    ids=[
-        "message_template",
-        "no_message_template",
-    ],
-)
-# fmt: on
-@pytest.mark.benchmark(group="syslog_monitor")
-def test_handle_syslog_logs(benchmark, message_template):
-    """
-    Right now this micro benchmarks just measures how much overhead parsing message takes, but it
-    doesn't really measure how much overhead other things such as logging, managing log watchers,
-    etc. add.
-    """
-    if message_template:
-        message_log_template = "test-$PROTO-$SRCIP.txt"
-    else:
-        message_log_template = None
-
-    mock_logger = mock.Mock()
-    mock_line_reporter = mock.Mock()
-    mock_config = MockConfig(
-        values={
-            "expire_log": 300,
-            "check_for_unused_logs_mins": 100,
-            "check_for_unused_logs_hours": 10,
-            "delete_unused_logs_hours": 10,
-            "check_rotated_timestamps": False,
-            "max_log_files": 1000,
-            "message_log_template": message_log_template,
+    for i in range(0, 1000):
+        extra = {
+            "proto": FAKER.random_element(["tcp", "udp"]),
+            "srcip": "127.0.0." + str(FAKER.random_int(0, 250)),
+            "destport": str(FAKER.random_int(10000, 60000))
         }
+        MOCK_EXTRAS.append(extra)
+
+
+    # fmt: off
+    @pytest.mark.parametrize(
+        "message_template",
+        [
+            True,
+            False,
+        ],
+        ids=[
+            "message_template",
+            "no_message_template",
+        ],
     )
-
-    with tempfile.TemporaryDirectory() as tmp_directory:
-        handler = SyslogHandler(logger=mock_logger, line_reporter=mock_line_reporter, config=mock_config,
-                                server_host="localhost", log_path=tmp_directory, get_log_watcher=mock.Mock(),
-                                rotate_options=(2, 200000), docker_options=mock.Mock())
-        handler._SyslogHandler__get_log_watcher.return_value = mock.Mock(), mock.Mock()
-
-        assert len(os.listdir(tmp_directory)) == 0
-
-        def run_benchmark():
-            extra = FAKER.random_element(MOCK_EXTRAS)
-            handler.handle(MOCK_MESSAGE, extra=extra)
-
-        benchmark.pedantic(run_benchmark, iterations=200, rounds=100)
-
-        # Verify that files which match message template notation have been created
-        file_names = os.listdir(tmp_directory)
-
+    # fmt: on
+    @pytest.mark.benchmark(group="syslog_monitor")
+    def test_handle_syslog_logs(benchmark, message_template):
+        """
+        Right now this micro benchmarks just measures how much overhead parsing message takes, but it
+        doesn't really measure how much overhead other things such as logging, managing log watchers,
+        etc. add.
+        """
         if message_template:
-            assert len(file_names) >= 100
-            for file_name in file_names:
-                assert (file_name.startswith("test-tcp-") or file_name.startswith("test-udp-"))
+            message_log_template = "test-$PROTO-$SRCIP.txt"
         else:
-            assert len(file_names) == 0
+            message_log_template = None
+
+        mock_logger = mock.Mock()
+        mock_line_reporter = mock.Mock()
+        mock_config = MockConfig(
+            values={
+                "expire_log": 300,
+                "check_for_unused_logs_mins": 100,
+                "check_for_unused_logs_hours": 10,
+                "delete_unused_logs_hours": 10,
+                "check_rotated_timestamps": False,
+                "max_log_files": 1000,
+                "message_log_template": message_log_template,
+            }
+        )
+
+        with tempfile.TemporaryDirectory() as tmp_directory:
+            handler = SyslogHandler(logger=mock_logger, line_reporter=mock_line_reporter, config=mock_config,
+                                    server_host="localhost", log_path=tmp_directory, get_log_watcher=mock.Mock(),
+                                    rotate_options=(2, 200000), docker_options=mock.Mock())
+            handler._SyslogHandler__get_log_watcher.return_value = mock.Mock(), mock.Mock()
+
+            assert len(os.listdir(tmp_directory)) == 0
+
+            def run_benchmark():
+                extra = FAKER.random_element(MOCK_EXTRAS)
+                handler.handle(MOCK_MESSAGE, extra=extra)
+
+            benchmark.pedantic(run_benchmark, iterations=200, rounds=100)
+
+            # Verify that files which match message template notation have been created
+            file_names = os.listdir(tmp_directory)
+
+            if message_template:
+                assert len(file_names) >= 100
+                for file_name in file_names:
+                    assert (file_name.startswith("test-tcp-") or file_name.startswith("test-udp-"))
+            else:
+                assert len(file_names) == 0
