484f608b8a607a2e80d8a4c59ea3e50abd2c2362
arthur <arthurk@sentinelone.com>
2023-03-02 20:49:51 +0100

    fix pre-uninstall cleanup.

--- agent_build_refactored/managed_packages/install-scriptlets/preuninstall.sh
+++ agent_build_refactored/managed_packages/install-scriptlets/preuninstall.sh
@@ -42,8 +42,8 @@ fi
 
 # Always remove the .pyc files and __pycache__ directories
 
-# Removing only directories causes repeated deletion of some directories, which lead to warning messages,
-# so we delete files in the pycache folders first and only then the folders themselves.
+# It should be more logical to remove only  the __pycache__ directories, but that causes repeated deletion,
+# which leads to warning messages, so we delete files and directories in different conditions,
 find /usr/share/scalyr-agent-2 -type f -path "*/__pycache__/*" -delete -o -type d  -name __pycache__ -delete
 find /opt/scalyr-agent-2 -type f -path "*/__pycache__/*" -delete -o -type d  -name __pycache__ -delete
 
