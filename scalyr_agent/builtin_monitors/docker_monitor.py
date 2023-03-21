22a9424f0f70f6ad6a8d338c6c55bbdbc8a18acf
Tomaz Muraus <tomaz@tomaz.me>
2023-03-21 10:49:27 +0100

    Fix logging bug in the Docker monitor (#1098)
    * Add a rate limit to the log message to ensure we don't exhaust CPU
    cycles in case we end up in a scenario where that message would be
    logged very often.
    
    * Fix lint.

--- scalyr_agent/builtin_monitors/docker_monitor.py
+++ scalyr_agent/builtin_monitors/docker_monitor.py
@@ -1634,7 +1634,14 @@ class DockerLogger(object):
                         # split the docker timestamp from the frest of the line
                         dt, log_line = _split_datetime_from_line(line)
                         if not dt:
-                            global_log.error("No timestamp found on line: '%s'", line)
+                            # Under some edge cases this message can be logged a lot (which can
+                            # exhaust the CPU) so we need to make sure rate limit is in place.
+                            global_log.error(
+                                "No timestamp found on line: '%s'",
+                                line,
+                                limit_once_per_x_secs=300,
+                                limit_key="docker-monitor-line-missing-ts",
+                            )
                         else:
                             timestamp = scalyr_util.seconds_since_epoch(dt, epoch)
 
