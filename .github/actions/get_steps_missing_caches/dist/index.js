c44e2f3d502723d2bc4d8a009353505f586d8aa3
arthur <arthurk@sentinelone.com>
2023-03-31 18:43:37 +0200

    test

--- .github/actions/get_steps_missing_caches/dist/index.js
+++ .github/actions/get_steps_missing_caches/dist/index.js
@@ -59859,10 +59859,11 @@ async function executeRunner() {
 
     for (let key of cachesKeys) {
         console.log(`Check cache for step cache key ${key}`);
-//        const result = await cache.restoreCache(
-//            primaryKey=key,
-//            options={ lookupOnly: true }
-//        )
+        const result = await cache.restoreCache(
+            "",
+            primaryKey=key,
+            options={ lookupOnly: true }
+        )
         console.log(result)
     }
 
