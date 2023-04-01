25c251b517561c77a9239d1fb867db7add41558f
arthur <arthurk@sentinelone.com>
2023-04-01 18:53:38 +0200

    test

--- .github/actions/get_steps_missing_caches/index.js
+++ .github/actions/get_steps_missing_caches/index.js
@@ -30,7 +30,8 @@ async function executeRunner() {
 
     const stepsIdsJSON = core.getInput("steps_ids");
     const lookupOnlyStr = core.getInput("lookup-only");
-    const lookupOnly = lookupOnlyStr === "true" ? true : false;
+    const lookupOnly = lookupOnlyStr === 'true' ? true : false;
+    console.log(lookupOnlyStr === 'true')
     const cacheRoot = core.getInput("cache_root");
 
 
