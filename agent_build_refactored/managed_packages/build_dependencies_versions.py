10afc2c561c78472a1a5058de094b2abb79c09eb
Tomaz Muraus <tomazm@sentinelone.com>
2023-04-28 09:28:46 +0200

    Upgrade AIO to 3.11.3.

--- agent_build_refactored/managed_packages/build_dependencies_versions.py
+++ agent_build_refactored/managed_packages/build_dependencies_versions.py
@@ -17,7 +17,7 @@ This module defines version for Python interpreter that is used in our dependenc
 of some of its dependencies.
 """
 
-EMBEDDED_PYTHON_VERSION = "3.11.1"
+EMBEDDED_PYTHON_VERSION = "3.11.3"
 
 # Versions of OpenSSL libraries to build for Python.
 PYTHON_PACKAGE_SSL_1_1_1_VERSION = "1.1.1s"
