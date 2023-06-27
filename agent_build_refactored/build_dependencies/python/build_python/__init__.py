3ae1ab27398b774ca3f6d37e5b57c9c3967f04c5
arthur <arthurk@sentinelone.com>
2023-06-27 12:11:25 +0200

    test

--- agent_build_refactored/build_dependencies/python/build_python/__init__.py
+++ agent_build_refactored/build_dependencies/python/build_python/__init__.py
@@ -5,7 +5,7 @@ from agent_build_refactored.tools.builder import BuilderStep
 from agent_build_refactored.build_dependencies.python.prepare_build_base import PrepareBuildBaseStep
 from agent_build_refactored.build_dependencies.python.build_python_dependencies.download_sources_base import DownloadSourcesBaseStep
 from agent_build_refactored.build_dependencies.python.build_python_dependencies import (
-    BuildPythonXZStep,
+    BuildXZStep,
     BuildPythonOpenSSLStep,
     BuildPythonSqliteStep,
     BuildPythonZlibStep,
@@ -31,10 +31,16 @@ class BuilderPythonStep(BuilderStep):
     def __init__(
         self,
         python_version: str,
+        openssl_version: str,
         openssl_type: str,
         architecture: CpuArch,
         libc: str,
+        install_prefix: pl.Path = None,
     ):
+
+        self.architecture = architecture
+        self.libc = libc
+        self.install_prefix = install_prefix or pl.Path("/opt/scalyr-agent-2/python3")
         self.prepare_build_base_step = PrepareBuildBaseStep(
             architecture=architecture,
             libc=libc
@@ -42,7 +48,7 @@ class BuilderPythonStep(BuilderStep):
 
         self.download_base_step = DownloadSourcesBaseStep()
 
-        self.build_xz_step = BuildPythonXZStep(
+        self.build_xz_step = BuildXZStep(
             xz_version="5.2.6",
             architecture=architecture,
             libc=libc,
@@ -91,20 +97,16 @@ class BuilderPythonStep(BuilderStep):
             libc=libc,
         )
 
-        openssl_install_prefix = pl.Path("/opt/scalyr-agent-2/openssl")
+        # self.build_openssl_1_step = BuildPythonOpenSSLStep(
+        #     version="1.1.1s",
+        #     architecture=architecture,
+        #     libc=libc,
+        # )
 
-        self.build_openssl_1_step = BuildPythonOpenSSLStep(
-            version="1.1.1s",
+        self.build_openssl_step = BuildPythonOpenSSLStep(
+            version=openssl_version,
             architecture=architecture,
             libc=libc,
-            install_prefix=openssl_install_prefix,
-        )
-
-        self.build_openssl_3_step = BuildPythonOpenSSLStep(
-            version="3.0.7",
-            architecture=architecture,
-            libc=libc,
-            install_prefix=openssl_install_prefix,
         )
 
         python_x_y_version = _get_python_x_y_version(version=python_version)
@@ -124,25 +126,13 @@ class BuilderPythonStep(BuilderStep):
                 self.build_ncurses_step,
                 self.build_libedit_step,
                 self.build_libffi_step,
-                self.build_openssl_1_step,
-                self.build_openssl_3_step,
+                #self.build_openssl_1_step,
+                self.build_openssl_step,
             ],
             build_args={
-                # "XZ_VERSION": "5.2.6",
-                # "ZLIB_VERSION": "1.2.13",
-                # "BZIP_VERSION": "1.0.8",
-                # "UTIL_LINUX_VERSION": "2.38",
-                # "NCURSES_VERSION": "6.3",
-                # "LIBFFI_VERSION": "3.4.2",
-                # "OPENSSL_1_VERSION": "1.1.1s",
-                # "OPENSSL_3_VERSION": "3.0.7",
-                # "LIBEDIT_VERSION_COMMIT": "0cdd83b3ebd069c1dee21d81d6bf716cae7bf5da",  # tag - "upstream/3.1-20221030",
-                # "TCL_VERSION_COMMIT": "338c6692672696a76b6cb4073820426406c6f3f9",  # tag - "core-8-6-13"}",
-                # "SQLITE_VERSION_COMMIT": "e671c4fbc057f8b1505655126eaf90640149ced6",  # tag - "version-3.41.2",
                 "PYTHON_VERSION": python_version,
                 "PYTHON_X_Y_VERSION": python_x_y_version,
-                "INSTALL_PREFIX": "/opt/scalyr-agent-2/python3",
-                "OPENSSL_INSTALL_PREFIX": str(openssl_install_prefix),
+                "INSTALL_PREFIX": str(self.install_prefix),
                 "OPENSSL_TYPE": openssl_type,
                 "COMMON_PYTHON_DEPENDENCY_INSTALL_PREFIX": COMMON_PYTHON_DEPENDENCY_INSTALL_PREFIX,
                 "ARCH": architecture.value,
