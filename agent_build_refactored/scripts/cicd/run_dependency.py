09f9e20458457053c6a52367519fbb849bb6b61a
arthur <arthurk@sentinelone.com>
2023-07-05 10:56:46 +0200

    test

--- agent_build_refactored/scripts/cicd/run_dependency.py
+++ agent_build_refactored/scripts/cicd/run_dependency.py
@@ -1,4 +1,11 @@
 import argparse
+import sys
+import pathlib as pl
+
+
+# Add source root to the PYTHONPATH in order to be able to import
+# local packages. All such imports also have to be done after that.
+sys.path.append(str(pl.Path(__file__).parent.parent.parent.parent))
 
 from agent_build_refactored.scripts.cicd import all_dependencies
 
