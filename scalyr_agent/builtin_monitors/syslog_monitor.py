1245df039d0402d7b6a1b13536e64203d19c815f
alesnovak-s1 <138495238+alesnovak-s1@users.noreply.github.com>
2024-01-10 11:10:57 +0100

    Removing unused queue import, which is not supported in python2. concurrent.futures imported only for PY3 (#1230)

--- scalyr_agent/builtin_monitors/syslog_monitor.py
+++ scalyr_agent/builtin_monitors/syslog_monitor.py
@@ -25,8 +25,6 @@ from __future__ import absolute_import
 
 from __future__ import print_function
 
-import concurrent.futures
-import queue
 
 from scalyr_agent import compat
 
@@ -58,6 +56,7 @@ import six.moves.socketserver
 if six.PY2:
     from scalyr_agent.builtin_monitors.thread_pool_dummy import ExecutorMixIn
 else:
+    import concurrent.futures
     from scalyr_agent.builtin_monitors.thread_pool import ExecutorMixIn
 
 try:
