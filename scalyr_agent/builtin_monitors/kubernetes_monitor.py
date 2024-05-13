2664e267a1f1d8238d96c08b71315c2b0600f351
Joseph Makar <joseph.makar@sentinelone.com>
2024-05-13 15:03:07 -0400

    DTIN-4234: Moved docs to scalyr_agent/builtin_monitors/kubernetes_monitor.py

--- scalyr_agent/builtin_monitors/kubernetes_monitor.py
+++ scalyr_agent/builtin_monitors/kubernetes_monitor.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Scalyr Inc.
+# Copyright 2018-2024 Scalyr Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,8 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ------------------------------------------------------------------------
-# author:  Imron Alston <imron@scalyr.com>
 
 from __future__ import unicode_literals
 from __future__ import absolute_import
