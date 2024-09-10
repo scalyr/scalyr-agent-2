636a9248c82cc78db7dcdeddaf3189e7d4120d8a
ales.novak <ales.novak@sentinelone.com>
2024-09-10 17:30:19 +0200

    socketserver python2 import fix

--- scalyr_agent/builtin_monitors/thread_pool_py2.py
+++ scalyr_agent/builtin_monitors/thread_pool_py2.py
@@ -16,7 +16,7 @@
 # ------------------------------------------------------------------------
 # @author ales.novak@sentinelone.com
 
-from SocketServer import ThreadingMixIn
+from six.moves.socketserver import ThreadingMixIn
 
 from scalyr_agent import scalyr_logging
 
