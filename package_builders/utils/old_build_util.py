234e4f278a6871e380c90f713c3cd16d27c15fc6
Steven Czerwinski <stevenc@sentinelone.com>
2026-05-11 12:45:58 -0700

    Add comment about need to eliminate old_build_util.py

--- package_builders/utils/old_build_util.py
+++ package_builders/utils/old_build_util.py
@@ -1,3 +1,25 @@
+# Copyright 2026 Scalyr Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# TODO: Eliminate this file by updating any builder referencing it to the "new" building
+# utilities.
+#
+# This file contains definitions for the functions previously used by the
+# original `build_packages.py` script. Any builder referencing these functions have
+# not been fully migrated over to the "new" building approach. When we have time, we should
+# move them over.
+
 import errno
 import glob
 import json
@@ -11,6 +33,7 @@ import tempfile
 import uuid
 from io import StringIO
 from time import strftime, gmtime
+
 
 _source_root_ = pl.Path(__file__).parent.parent.parent.absolute()
 
